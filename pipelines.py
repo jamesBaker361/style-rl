03b95a284536dc4c9ea8e9d5e5623925aad90103
James Baker <jlbaker361@gmail.com>
2025-02-06 14:49:30 -0500

    created helper

---
+++ pipelines.py
@@ -0,0 +1,13 @@
+from trl import DDPOConfig, DDPOTrainer, DefaultDDPOStableDiffusionPipeline,DDPOPipelineOutput,DDPOStableDiffusionPipeline
+from diffusers import DiffusionPipeline
+
+class KeywordDDPOStableDiffusionPipeline(DefaultDDPOStableDiffusionPipeline):
+    def __init__(self,sd_pipeline:DiffusionPipeline,keyword:str=""):
+        self.sd_pipeline=sd_pipeline
+        self.keyword=keyword
+
+    def get_trainable_layers(self):
+        return  [
+        p for name, p in self.sd_pipeline.unet.named_parameters()
+        if p.requires_grad and self.keyword in name
+            ]

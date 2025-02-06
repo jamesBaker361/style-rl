b0a3a4c320e777e1f44bbeb5f46e5751d039ab23
James Baker <jlbaker361@gmail.com>
2025-02-06 11:22:07 -0500

    get vit embeddings

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import os
 import argparse
 from experiment_helpers.gpu_details import print_details
+from experiment_helpers.better_vit_model import BetterViTModel
+from transformers import CLIPProcessor, CLIPModel,ViTImageProcessor, ViTModel,CLIPTokenizer
 from accelerate import Accelerator
 from diffusers import DiffusionPipeline
+from datasets import load_dataset
 import torch
 import time
 
@@ -13,10 +16,31 @@ parser.add_argument("--project_name",type=str,default="style_creative")
 parser.add_argument("--prompt",type=str,default="wizard")
 parser.add_argument("--style_dataset",type=str,default="jlbaker361/stylization")
 parser.add_argument("--start",type=int,default=0)
-parser.add_argument("--end",type=int,default=5)
+parser.add_argument("--limit",type=int,default=5)
 parser.add_argument("--method",type=str,default="ddpo")
 parser.add_argument("--image_dim",type=int,default=256)
 parser.add_argument("--num_inference_steps",type=int,default=4)
+
+def get_vit_embeddings(vit_processor: ViTImageProcessor, vit_model: BetterViTModel, image_list:list,return_numpy:bool=True):
+    '''
+    returns (vit_embedding_list,vit_style_embedding_list, vit_content_embedding_list)
+    '''
+    vit_embedding_list=[]
+    vit_content_embedding_list=[]
+    vit_style_embedding_list=[]
+    for image in image_list:
+        vit_inputs = vit_processor(images=[image], return_tensors="pt")
+        #print("inputs :)")
+        vit_inputs['pixel_values']=vit_inputs['pixel_values'].to(vit_model.device)
+        vit_outputs=vit_model(**vit_inputs,output_hidden_states=True, output_past_key_values=True)
+        vit_embedding_list.append(vit_outputs.last_hidden_state.reshape(1,-1)[0])
+        vit_style_embedding_list.append(vit_outputs.last_hidden_state[0][0]) #CLS token: https://github.com/google/dreambooth/issues/3
+        vit_content_embedding_list.append(vit_outputs.past_key_values[11][0].reshape(1,-1)[0])
+    if return_numpy:
+        vit_embedding_list=[v.cpu().numpy() for v in vit_embedding_list]
+        vit_style_embedding_list=[v.cpu().numpy() for v in vit_style_embedding_list]
+        vit_content_embedding_list=[v.cpu().numpy() for v in vit_content_embedding_list]
+    return vit_embedding_list,vit_style_embedding_list, vit_content_embedding_list
 
 
 
@@ -35,10 +59,39 @@ def main(args):
 
     prompt = "Self-portrait oil painting, a beautiful cyborg with golden hair, 8k"
 
+    try:
+        vit_processor = ViTImageProcessor.from_pretrained('facebook/dino-vitb16')
+        vit_model = BetterViTModel.from_pretrained('facebook/dino-vitb16')
+    except:
+    
+        vit_processor = ViTImageProcessor.from_pretrained('facebook/dino-vitb16',force_download=True)
+        vit_model = BetterViTModel.from_pretrained('facebook/dino-vitb16',force_download=True)
+    vit_model.eval()
+    vit_model.requires_grad_(False)
+
     # Can be set to 1~50 steps. LCM support fast inference even <= 4 steps. Recommend: 1~8 steps.
     num_inference_steps = args.num_inference_steps
-    images = pipe(prompt=prompt, num_inference_steps=num_inference_steps, guidance_scale=8.0,height=args.image_size,width=args.image_size).images
-    images[0].save("image.png")
+    #images = pipe(prompt=prompt, num_inference_steps=num_inference_steps, guidance_scale=8.0,height=args.image_size,width=args.image_size).images
+    #images[0].save("image.png")
+    data=load_dataset(args.style_dataset,split="train")
+    for i, row in enumerate(data):
+        if i<args.start or i>=args.limit:
+            continue
+        label=row["label"]
+        images=[row[f"image_{k}"] for k in range(4)]
+        vit_embedding_list,vit_style_embedding_list, vit_content_embedding_list=get_vit_embeddings(vit_processor,vit_model,images,False)
+        print(type(vit_style_embedding_list[0]))
+        try:
+            print("size",vit_style_embedding_list[0].size)
+        except:
+            try:
+                print("shape",vit_style_embedding_list[0].shape)
+            except:
+                try:
+                    print("len",len(vit_style_embedding_list[0]))
+                except:
+                    pass
+
     return
 
 if __name__=='__main__':

cb4b4024e2da4d5d6959f7863b15c7e53843d786
James Baker <jlbaker361@gmail.com>
2025-04-18 16:05:30 -0400

    boilerplate stuff

--- main_pers.py
+++ main_pers.py
@@ -4,12 +4,27 @@ from experiment_helpers.gpu_details import print_details
 from experiment_helpers.better_vit_model import BetterViTModel
 from experiment_helpers.better_ddpo_trainer import BetterDDPOTrainer
 from experiment_helpers.unsafe_stable_diffusion_pipeline import UnsafeStableDiffusionPipeline
+from datasets import load_dataset
+import torch
+from accelerate import Accelerator
 import time
 
 parser=argparse.ArgumentParser()
+parser.add_argument("--dataset",type=str,default="jlbaker361/captioned-images")
+parser.add_argument("--mixed_precision",type=str,default="no")
+parser.add_argument("--project_name",type=str,default="person")
+parser.add_argument("--gradient_accumulation_steps",type=int,default=4)
 
 def main(args):
-    pass
+    accelerator=Accelerator(log_with="wandb",mixed_precision=args.mixed_precision,gradient_accumulation_steps=args.gradient_accumulation_steps)
+    print("accelerator device",accelerator.device)
+    accelerator.init_trackers(project_name=args.project_name,config=vars(args))
+    torch_dtype={
+        "no":torch.float32,
+        "fp16":torch.float16,
+        "bf16":torch.bfloat16
+    }[args.mixed_precision]
+
 
 if __name__=='__main__':
     print_details()

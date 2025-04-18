24b2afd732b270a62a63c84690e2c92143a793cc
James Baker <jlbaker361@gmail.com>
2025-04-18 19:50:26 -0400

    device stuff

--- make_captioned_dataset.py
+++ make_captioned_dataset.py
@@ -20,10 +20,9 @@ parser.add_argument("--limit",type=int,default=-1)
 
 def main(args):
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    torch_dtype={
-        "cuda":torch.float16,
-        "cpu":torch.float32
-    }[device]
+    torch_dtype=torch.float32
+    if torch.cuda.is_available():
+        torch_dtype=torch.float16
 
     processor = BlipProcessor.from_pretrained("Salesforce/blip-image-captioning-large")
     model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-large").to(device,torch_dtype)

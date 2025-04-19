6d4c76827b2cd0862574e742700d98125f75c4be
James Baker <jlbaker361@gmail.com>
2025-04-19 00:32:39 -0400

    req grad

--- main_ir.py
+++ main_ir.py
@@ -237,9 +237,12 @@ def main(args):
         print("before ",sd_pipeline.unet.config.sample_size)
         sd_pipeline.unet.config.sample_size=args.image_size // sd_pipeline.vae_scale_factor
         print("after", sd_pipeline.unet.config.sample_size)
-        sd_pipeline.unet.to(accelerator.device).requires_grad_(False)
+        sd_pipeline.unet.to(accelerator.device)
+        sd_pipeline.unet.requires_grad_(False)
         sd_pipeline.text_encoder.to(accelerator.device).requires_grad_(False)
+        sd_pipeline.text_encoder.requires_grad_(False)
         sd_pipeline.vae.to(accelerator.device).requires_grad_(False)
+        sd_pipeline.vae.requires_grad_(False)
         tokenizer=sd_pipeline.tokenizer
         text_encoder=sd_pipeline.text_encoder
 

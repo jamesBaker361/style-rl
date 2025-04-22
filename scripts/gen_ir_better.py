9cf33fd14ddd33e2f5c7e60b353fd4897643706d
James Baker <jlbaker361@gmail.com>
2025-04-22 14:48:15 -0400

    qualiclip

--- scripts/gen_ir_better.py
+++ scripts/gen_ir_better.py
@@ -2,18 +2,19 @@ exclude=" --exclude=gpu[005,006,008,010-014,017,018],cuda[001-008],pascal[001-01
 for lr in [0.001,0.0001]:
     for unet in ["no-unet","unet"]:
         for pplus in ["no-pplus","pplus"]:
-            for num_vectors in [1,3]:
-                name=f"ti_{lr}_{unet}_{pplus}_{num_vectors}"
-                unet_flag={
-                    "no-unet":"",
-                    "unet":"--train_unet"
-                }[unet]
-                pplus_flag={
-                    "no-pplus":"",
-                    "pplus": "--use_pplus"
-                }[pplus]
-                command=f"sbatch -J ir-ti --err=slurm/ir/{name}.err --out=slurm/ir/{name}.out"
-                command+=f" {exclude} runpygpu.sh main_ir.py  --epochs 10000  {unet_flag} {pplus_flag} --num_vectors {num_vectors}"
-                command+=f"   --project_name ti-ir --textual_inversion --prompt_src_txt random_animals.txt"
-                command+=f" --validation_epochs 40 --learning_rate {lr}"
-                print(command)
+            for num_vectors in [1]:
+                for reward_fn in ["ir","qualiclip"]:
+                    name=f"ti_{reward_fn}_{lr}_{unet}_{pplus}_{num_vectors}"
+                    unet_flag={
+                        "no-unet":"",
+                        "unet":"--train_unet"
+                    }[unet]
+                    pplus_flag={
+                        "no-pplus":"",
+                        "pplus": "--use_pplus"
+                    }[pplus]
+                    command=f"sbatch -J {reward_fn}-ti --err=slurm/ti/{name}.err --out=slurm/ti/{name}.out"
+                    command+=f" {exclude} runpygpu.sh main_ir.py  --epochs 10000  {unet_flag} {pplus_flag} --num_vectors {num_vectors}"
+                    command+=f"   --project_name {reward_fn} --textual_inversion --prompt_src_txt random_animals.txt"
+                    command+=f" --validation_epochs 40 --learning_rate {lr} --reward_fn {reward_fn} "
+                    print(command)

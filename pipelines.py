3ee4d50330da59e42ce0295db51f5bf982a3338f
James Baker <jlbaker361@gmail.com>
2025-04-22 21:27:21 -0400

    new output

--- pipelines.py
+++ pipelines.py
@@ -23,6 +23,28 @@ from diffusers.utils import (
 from diffusers.models.lora import adjust_lora_scale_text_encoder
 from gpu_helpers import *
 from p_unet import PPlusUNet2DConditionModel
+from diffusers.utils.outputs import BaseOutput
+from dataclasses import dataclass
+import PIL
+import numpy as np
+
+@dataclass
+class CustomStableDiffusionPipelineOutput(BaseOutput):
+    """
+    Output class for Stable Diffusion pipelines.
+
+    Args:
+        images (`List[PIL.Image.Image]` or `np.ndarray`)
+            List of denoised PIL images of length `batch_size` or NumPy array of shape `(batch_size, height, width,
+            num_channels)`.
+        nsfw_content_detected (`List[bool]`)
+            List indicating whether the corresponding generated image contains "not-safe-for-work" (nsfw) content or
+            `None` if safety checking could not be performed.
+    """
+
+    images: Union[List[PIL.Image.Image], np.ndarray]
+    nsfw_content_detected: Optional[List[bool]]
+    latents: Optional[torch.Tensor]
     
 
 class CompatibleLatentConsistencyModelPipeline(LatentConsistencyModelPipeline):

901bbfd520946d053dec6dad45f56dbb58640f62
James Baker <jlbaker361@gmail.com>
2025-04-24 12:08:02 -0400

    pers

--- pers.sh
+++ pers.sh
@@ -1,4 +1,4 @@
 
 sbatch -J pers --err=slurm/main/pers-ssl.err --out=slurm/main/pers-ssl.out runpymain.sh main_pers.py --epochs 2 --embedding ssl
 
-sbatch -J pers --err=slurm/main/pers-ssl.err --out=slurm/main/pers-ssl.out runpymain.sh main_pers.py --epochs 2 --embedding ssl --training_type reward
+sbatch -J pers --err=slurm/main/pers-ssl-reward.err --out=slurm/main/pers-ssl-reward.out runpymain.sh main_pers.py --epochs 2 --embedding ssl --training_type reward

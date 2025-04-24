0cd9aca61c571b55d701ccbb20fc6b4da73c0abb
James Baker <jlbaker361@gmail.com>
2025-04-24 18:37:54 -0400

    gpu

--- pers.sh
+++ pers.sh
@@ -3,3 +3,9 @@ sbatch -J pers --err=slurm/main/pers-ssl.err --out=slurm/main/pers-ssl.out runpy
 sbatch -J pers --err=slurm/main/pers-ssl-velocity.err --out=slurm/main/pers-ssl-velocity.out runpymain.sh main_pers.py --epochs 2 --embedding ssl --prediction_type velocity --dataset jlbaker361/captioned-test --validation_interval 1 --train_split 0.5
 
 sbatch -J pers --err=slurm/main/pers-ssl-reward.err --out=slurm/main/pers-ssl-reward.out runpymain.sh main_pers.py --epochs 2 --embedding ssl --training_type reward --dataset jlbaker361/captioned-test --validation_interval 1 --train_split 0.5
+
+
+sbatch -J pers --err=slurm/main/gpu-pers-ssl.err --out=slurm/main/gpu-pers-ssl.out runpygpu.sh main_pers.py --epochs 2 --embedding ssl --dataset jlbaker361/captioned-test --validation_interval 1 --train_split 0.5
+sbatch -J pers --err=slurm/main/gpu-pers-ssl-velocity.err --out=slurm/main/gpu-pers-ssl-velocity.out runpygpu.sh main_pers.py --epochs 2 --embedding ssl --prediction_type velocity --dataset jlbaker361/captioned-test --validation_interval 1 --train_split 0.5
+
+sbatch -J pers --err=slurm/main/gpu-pers-ssl-reward.err --out=slurm/main/gpu-pers-ssl-reward.out runpygpu.sh main_pers.py --epochs 2 --embedding ssl --training_type reward --dataset jlbaker361/captioned-test --validation_interval 1 --train_split 0.5

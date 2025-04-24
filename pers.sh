4ce0eca9da1af8b86d057bd3cf5c5132d97c753c
James Baker <jlbaker361@gmail.com>
2025-04-24 18:13:25 -0400

    nice

--- pers.sh
+++ pers.sh
@@ -1,5 +1,5 @@
 
-sbatch -J pers --err=slurm/main/pers-ssl.err --out=slurm/main/pers-ssl.out runpymain.sh main_pers.py --epochs 2 --embedding ssl --dataset jlbaker361/captioned-test
-sbatch -J pers --err=slurm/main/pers-ssl-velocity.err --out=slurm/main/pers-ssl-velocity.out runpymain.sh main_pers.py --epochs 2 --embedding ssl --prediction_type velocity --dataset jlbaker361/captioned-test
+sbatch -J pers --err=slurm/main/pers-ssl.err --out=slurm/main/pers-ssl.out runpymain.sh main_pers.py --epochs 2 --embedding ssl --dataset jlbaker361/captioned-test --validation_interval 1 --train_split 0.5
+sbatch -J pers --err=slurm/main/pers-ssl-velocity.err --out=slurm/main/pers-ssl-velocity.out runpymain.sh main_pers.py --epochs 2 --embedding ssl --prediction_type velocity --dataset jlbaker361/captioned-test --validation_interval 1 --train_split 0.5
 
-sbatch -J pers --err=slurm/main/pers-ssl-reward.err --out=slurm/main/pers-ssl-reward.out runpymain.sh main_pers.py --epochs 2 --embedding ssl --training_type reward --dataset jlbaker361/captioned-test
+sbatch -J pers --err=slurm/main/pers-ssl-reward.err --out=slurm/main/pers-ssl-reward.out runpymain.sh main_pers.py --epochs 2 --embedding ssl --training_type reward --dataset jlbaker361/captioned-test --validation_interval 1 --train_split 0.5

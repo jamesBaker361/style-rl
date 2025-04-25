c8620d90e7266bc0abe1550ba4721fc4bb382e79
James Baker <jlbaker361@gmail.com>
2025-04-25 03:25:05 -0400

    down

---
+++ download.py
@@ -0,0 +1,4 @@
+from datasets import load_dataset
+
+for data in ["league_captioned_tile","league_captioned_splash","coco_captioned","art_coco_captioned","celeb_captioned"]:
+    load_dataset(f"jlbaker361/{data}",force_download=True)

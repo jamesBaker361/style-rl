59f8b5b14ab8c31ee8526f2c10850fdb31c360ec
James Baker <jlbaker361@gmail.com>
2025-04-25 03:12:59 -0400

    nice

--- main_pers.py
+++ main_pers.py
@@ -144,6 +144,8 @@ def main(args):
                 embedding=cls_features
             elif args.embedding=="siglip2":
                 inputs = processor(text=[""], images=img_tensor, padding="max_length", max_length=64, return_tensors="pt")
+                for key in ['input_ids','pixel_values']:
+                    print(key,inputs[key].device)
                 outputs = model(**inputs)
                 embedding=outputs.image_embeds
                 

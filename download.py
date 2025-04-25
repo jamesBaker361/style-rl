772759481b1740e1014b2a322a5017b4e37999f0
James Baker <jlbaker361@gmail.com>
2025-04-25 04:21:42 -0400

    no more froce donwload

--- download.py
+++ download.py
@@ -1,5 +1,5 @@
 from datasets import load_dataset
 
 for data in ["league_captioned_tile","league_captioned_splash","coco_captioned","art_coco_captioned","celeb_captioned"]:
-    load_dataset(f"jlbaker361/{data}",split="train",force_download=True)
+    load_dataset(f"jlbaker361/{data}",split="train")
     #break

49e060289f4122765d41e9a5cb52bfe60cfd2be8
James Baker <jlbaker361@gmail.com>
2025-04-25 00:51:13 -0400

    v prediction

--- pers.sh
+++ pers.sh
@@ -1,11 +1,11 @@
 
 sbatch -J pers --err=slurm/main/pers-ssl.err --out=slurm/main/pers-ssl.out runpymain.sh main_pers.py --epochs 2 --embedding ssl --dataset jlbaker361/captioned-test --validation_interval 1 --train_split 0.5 --image_size 128 --limit 8 --gradient_accumulation_steps 2
-sbatch -J pers --err=slurm/main/pers-ssl-velocity.err --out=slurm/main/pers-ssl-velocity.out runpymain.sh main_pers.py --epochs 2 --embedding ssl --prediction_type velocity --dataset jlbaker361/captioned-test --validation_interval 1 --train_split 0.5 --image_size 128 --limit 8 --gradient_accumulation_steps 2
+sbatch -J pers --err=slurm/main/pers-ssl-v_prediction.err --out=slurm/main/pers-ssl-v_prediction.out runpymain.sh main_pers.py --epochs 2 --embedding ssl --prediction_type v_prediction --dataset jlbaker361/captioned-test --validation_interval 1 --train_split 0.5 --image_size 128 --limit 8 --gradient_accumulation_steps 2
 
 sbatch -J pers --err=slurm/main/pers-ssl-reward.err --out=slurm/main/pers-ssl-reward.out runpymain.sh main_pers.py --epochs 2 --embedding ssl --training_type reward --dataset jlbaker361/captioned-test --validation_interval 1 --train_split 0.5 --image_size 128 --buffer_size 0 --limit 8 --gradient_accumulation_steps 2
 
 
 sbatch -J pers --err=slurm/main/gpu-pers-ssl.err --out=slurm/main/gpu-pers-ssl.out runpygpu.sh main_pers.py --epochs 2 --embedding ssl --dataset jlbaker361/captioned-test --validation_interval 1 --train_split 0.5 --image_size 128 --mixed_precision bf16 --limit 8 --gradient_accumulation_steps 2
-sbatch -J pers --err=slurm/main/gpu-pers-ssl-velocity.err --out=slurm/main/gpu-pers-ssl-velocity.out runpygpu.sh main_pers.py --epochs 2 --embedding ssl --prediction_type velocity --dataset jlbaker361/captioned-test --validation_interval 1 --train_split 0.5 --image_size 128 --mixed_precision bf16 --limit 8 --gradient_accumulation_steps 2
+sbatch -J pers --err=slurm/main/gpu-pers-ssl-v_prediction.err --out=slurm/main/gpu-pers-ssl-v_prediction.out runpygpu.sh main_pers.py --epochs 2 --embedding ssl --prediction_type v_prediction --dataset jlbaker361/captioned-test --validation_interval 1 --train_split 0.5 --image_size 128 --mixed_precision bf16 --limit 8 --gradient_accumulation_steps 2
 
 sbatch -J pers --err=slurm/main/gpu-pers-ssl-reward.err --out=slurm/main/gpu-pers-ssl-reward.out runpygpu.sh main_pers.py --epochs 2 --embedding ssl --training_type reward --dataset jlbaker361/captioned-test --validation_interval 1 --train_split 0.5 --image_size 128 --buffer_size 0 --mixed_precision bf16 --limit 8 --gradient_accumulation_steps 2

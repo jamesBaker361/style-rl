b64f0d1e6b75435355525b9e0f32ac015e45425e
James Baker <jlbaker361@gmail.com>
2025-05-11 00:01:41 -0400

    gen tes

---
+++ scripts/gen_test.py
@@ -0,0 +1,11 @@
+for training_type in ["denoise","reward"]:
+    for prediction_type in ["epsilon","v_prediction"]:
+        for embedding in ["dino","ssl","siglip2"]:
+            if prediction_type=="epsilon" and training_type=="reward":
+                continue
+            name=f"{training_type}_{prediction_type}_{embedding}"
+            command=f"sbatch -J pers --err=slurm/test/{name}.err --out =slurm/test/{name}.out"
+            command+=" runpygpu.sh main_pers.py --epochs 2 --limit 10 --project_name testing-pers "
+            command+=f" --mixed_precision fp16 --prediction_type {prediction_type} "
+            command+=f" --embedding {embedding} --training_type {training_type} "
+            print(command)

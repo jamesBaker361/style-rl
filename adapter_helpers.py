bc9f6ce88b90ba7e4c748ea61859cac14bedcaec
James Baker <jlbaker361@gmail.com>
2025-05-22 20:11:09 -0400

    nit

--- adapter_helpers.py
+++ adapter_helpers.py
@@ -39,7 +39,7 @@ def replace_ip_attn(unet:UNet2DConditionModel
         setattr(module, "to_v_ip",new_v_ip)
 
     multi_ip_adapter=MultiIPAdapterImageProjection([ImageProjection(embedding_dim,cross_attention_dim,num_image_text_embeds)])
-    unet.encoder_hid_proj(multi_ip_adapter,embedding_dim,intermediate_embedding_dim)
+    unet.encoder_hid_proj= MultiIPAdapterImageProjectionWithVisualProjection(multi_ip_adapter,embedding_dim,intermediate_embedding_dim)
     unet.encoder_hid_proj.to(unet.device)
 
     return unet

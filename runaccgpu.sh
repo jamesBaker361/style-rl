09767fbcbe40ad52c1c5a2e4b4e4c71eb9b10f30
James Baker <jlbaker361@gmail.com>
2025-05-23 13:44:53 -0400

    correct acc

--- runaccgpu.sh
+++ runaccgpu.sh
@@ -12,7 +12,7 @@
 
 #SBATCH --gres=gpu:2
 
-#SBATCH --mem=64000                # Real memory (RAM) required (MB)
+#SBATCH --mem=128000                # Real memory (RAM) required (MB)
 
 #SBATCH --time=3-00:00:00           # Total run time limit (D-HH:MM:SS)
 
@@ -20,16 +20,21 @@
 
 #SBATCH --error=slurm/generic/%j.err   # STDERR output file (optional)
 
-#SBATCH --exclude=gpuk[001-012],gpu[005,006,010-014,017,018],cuda[001-008],pascal[001-010]
+#SBATCH --exclusive 
 
-#SBATCH --exclusive
+#SBATCH --exclude=gpu[005,006,011,018],cuda[001-008],pascal[006-010],gpuk[001-012]
+
+#SBATCH --gres-flags=enforce-binding
 
 day=$(date +'%m/%d/%Y %R')
 echo "gpu"  ${day} $SLURM_JOBID "node_list" $SLURM_NODELIST $@  "\n" >> jobs.txt
 module purge
+export MODULEPATH=$MODULEPATH:/projects/community/modulefiles
 module load intel/17.0.4
 #module load cudnn/7.0.3
-module load cuda/11.3
+module load gcc/10.3.0-pgarias
+module load cuda/12.1.0
+gcc --version
 eval "$(conda shell.bash hook)"
 conda activate deephands
 export PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:64
@@ -49,5 +54,13 @@ export BRAIN_DATA_DIR='/scratch/jlb638/brain-diffuser/data'
 export CUDA_LAUNCH_BLOCKING="1"
 export SCIKIT_LEARN_DATA="/scratch/jlb638/scikit-learn-data"
 export BRAIN_DATA_DIR="/scratch/jlb638/brain/data"
+echo "Running on: $(hostname)"
+echo "Allocated GPUs:"
+nvidia-smi
+echo "version"
+nvcc --version
 srun accelerate launch --config_file config.yaml  $@
 conda deactivate
+echo "Running on: $(hostname)"
+echo "Allocated GPUs:"
+nvidia-smi

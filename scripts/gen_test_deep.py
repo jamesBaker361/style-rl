4615bab65763ffc620cae0418920de4b472fc548
James Baker <jlbaker361@gmail.com>
2025-05-23 13:34:52 -0400

    whoops

--- scripts/gen_test_deep.py
+++ scripts/gen_test_deep.py
@@ -5,7 +5,7 @@ for training_type in ["denoise","reward"]:
                 continue
             name=f"{training_type}_{prediction_type}_{embedding}"
             command=f"sbatch  -J pers  --err=slurm/test_deep/{name}.err --out=slurm/test_deep/{name}.out"
-            command+=" runpygpu.sh main_pers.py --epochs 2 --limit 10 --project_name testing-pers "
+            command+=" runaccgpu.sh main_pers.py --epochs 2 --limit 10 --project_name testing-pers "
             command+=f" --mixed_precision fp16 --prediction_type {prediction_type} "
             command+=f" --embedding {embedding} --training_type {training_type} --dataset jlbaker361/{embedding}-art_coco_captioned"
             print(command)

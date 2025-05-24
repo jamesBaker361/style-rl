2ebb0438998102b5215e16e5c39f5922ca5364cd
James Baker <jlbaker361@gmail.com>
2025-05-23 20:44:48 -0400

    prining

--- main_pers.py
+++ main_pers.py
@@ -94,6 +94,7 @@ def split_list_by_ratio(lst, ratios=(0.8, 0.1, 0.1)):
 def main(args):
     if args.deepspeed:
         accelerator=Accelerator(log_with="wandb")
+        print("using deepspeed")
     else:
         accelerator=Accelerator(log_with="wandb",mixed_precision=args.mixed_precision,gradient_accumulation_steps=args.gradient_accumulation_steps)
     print("accelerator device",accelerator.device)

0d8b5dcde842b76a661c47c51df40b1da6cf0ed1
James Baker <jlbaker361@gmail.com>
2025-05-26 18:15:59 -0400

    call to device

--- pipelines.py
+++ pipelines.py
@@ -217,6 +217,8 @@ class CompatibleLatentConsistencyModelPipeline(LatentConsistencyModelPipeline):
                     model_pred=evil_twin_model_pred + self.evil_twin_guidance_scale* (model_pred-evil_twin_model_pred)
                 # compute the previous noisy sample x_t -> x_t-1
                 print('model_pred.device',model_pred.device,'t device',t.device,'latents',latents.device)
+                t=t.to(model_pred.device)
+                latents=latents.to(model_pred.device)
                 latents, denoised = self.scheduler.step(model_pred, t, latents, **extra_step_kwargs, return_dict=False)
                 if callback_on_step_end is not None:
                     callback_kwargs = {}

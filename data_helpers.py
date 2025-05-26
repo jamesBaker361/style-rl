1b350985f870ffeb8b0085d701995f764202af8f
James Baker <jlbaker361@gmail.com>
2025-05-26 11:03:54 -0400

    whoops

--- data_helpers.py
+++ data_helpers.py
@@ -2,7 +2,6 @@ import torch
 from torch.utils.data import Dataset
 from torchvision import datasets
 from torchvision.transforms import ToTensor
-import matplotlib.pyplot as plt
 from torchvision import transforms
 
 class CustomTripleDataset(Dataset):

6d73e83e7aad9d7f5679ab0fdddbafddd34411b2
James Baker <jlbaker361@gmail.com>
2025-05-26 19:54:30 -0400

    prompt stuff

--- main_pers.py
+++ main_pers.py
@@ -312,7 +312,8 @@ def main(args):
                 text_batch=[text_batch]
                 embeds_batch=embeds_batch.unsqueeze(0)
             image_embeds=embeds_batch #.unsqueeze(0)
-            
+            if b==1:
+                print("images",image_batch.size(),"text",text_batch.size(),"embeds",embeds_batch.size())
             image_batch=torch.clamp(image_batch, 0, 1)
             if baseline:
                 ip_adapter_image=F_v2.resize(image_batch, (224,224))

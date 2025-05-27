7e8746ecf86346a1af10557d03307cdae2f0b5fe
James Baker <jlbaker361@gmail.com>
2025-05-27 17:58:01 -0400

    printing

--- main_pers.py
+++ main_pers.py
@@ -478,6 +478,7 @@ def main(args):
                     #print('unet.encoder_hid_proj.device',unet.encoder_hid_proj.image_projection_layers[0].device)
                     if args.vanilla:
                         with accelerator.autocast():
+                            print("noisy latents",noisy_latents.device,"timesteps",timesteps.device,"encoder states",encoder_hidden_states.device,"image_embeds",image_embeds.device)
                             model_pred = unet(noisy_latents, timesteps, encoder_hidden_states, added_cond_kwargs=added_cond_kwargs,return_dict=False)[0]
                             loss = F.mse_loss(model_pred.float(), target.float(), reduction="mean")
                     else:

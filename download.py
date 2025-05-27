295bb72bf272b489cbdf4f6ebd707e15feca171f
James Baker <jlbaker361@gmail.com>
2025-05-27 13:28:15 -0400

    pipeline and clip

--- download.py
+++ download.py
@@ -1,5 +1,8 @@
 import torch
 import datasets
+from pipelines import CompatibleLatentConsistencyModelPipeline
+from transformers import CLIPModel, AutoProcessor
+
 for model_type in ["dino_vits8" ,"dino_vits16" , "dino_vitb8" , "dino_vitb16" , "vit_small_patch8_224" ,"vit_small_patch16_224" , "vit_base_patch8_224" , "vit_base_patch16_224"]:
     try:
         model = torch.hub.load('facebookresearch/dino:main', model_type)
@@ -9,3 +12,7 @@ for model_type in ["dino_vits8" ,"dino_vits16" , "dino_vitb8" , "dino_vitb16" , 
 for data in ["league_captioned_tile","league_captioned_splash","coco_captioned","art_coco_captioned","celeb_captioned"]:
     path=f"jlbaker361/{data}"
     datasets.load_dataset(path)
+
+pipeline=CompatibleLatentConsistencyModelPipeline.from_pretrained("SimianLuo/LCM_Dreamshaper_v7")
+clip_model = CLIPModel.from_pretrained("openai/clip-vit-base-patch32")
+clip_processor = AutoProcessor.from_pretrained("openai/clip-vit-base-patch32")

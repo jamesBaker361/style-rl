714d9134c992c436a8dcc32f3f59cb5bf9942748
James Baker <jlbaker361@gmail.com>
2025-05-30 13:07:51 -0400

    inaccuracy

--- scripts/gen_big_training.py
+++ scripts/gen_big_training.py
@@ -3,12 +3,13 @@ port=29601
 for training_type in  ["denoise","reward"]:
     for prediction_type in ["epsilon","v_prediction"]:
         for embedding in ["dino","ssl"]:
-            if prediction_type=="epsilon" and training_type=="reward":
-                continue
-            name=f"{training_type}_{prediction_type}_{embedding}"
-            port+=1
-            command=f"sbatch  -J pers  --err=slurm/test_deep/{name}.err --out=slurm/test_deep/{name}.out"
-            command+=f" runaccgpu.sh --multi_gpu --mixed_precision fp16 --num_processes 2 --main_process_port {port} main_pers.py --epochs 250 --limit -1 --batch_size 4 --project_name testing-pers "
-            command+=f" --mixed_precision fp16 --prediction_type {prediction_type} "
-            command+=f" --embedding {embedding} --training_type {training_type} --dataset jlbaker361/{embedding}-art_coco_captioned --vanilla --name jlbaker361/{name} --gradient_accumulation_steps 8 "
-            print(command)
+            for data in ["league_captioned_tile","league_captioned_splash","coco_captioned","art_coco_captioned","celeb_captioned"]:
+                if prediction_type=="epsilon" and training_type=="reward":
+                    continue
+                name=f"{training_type}_{prediction_type}_{embedding}"
+                port+=1
+                command=f"sbatch  -J pers  --err=slurm/pers_{data}/{name}.err --out=slurm/pers_{data}/{name}.out"
+                command+=f" runaccgpu.sh --multi_gpu --mixed_precision fp16 --num_processes 2 --main_process_port {port} main_pers.py --epochs 250 --limit -1 --batch_size 4 --project_name {data} "
+                command+=f" --mixed_precision fp16 --prediction_type {prediction_type} "
+                command+=f" --embedding {embedding} --training_type {training_type} --dataset jlbaker361/{embedding}-art_coco_captioned --vanilla --name jlbaker361/{name} --gradient_accumulation_steps 8 "
+                print(command)

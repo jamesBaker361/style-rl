f4e641eb7dc7bdb6ca6cb2cf247614760f381c99
James Baker <jlbaker361@gmail.com>
2025-05-30 09:01:58 -0400

    prompt optional

--- data_helpers.py
+++ data_helpers.py
@@ -5,7 +5,7 @@ from torchvision.transforms import ToTensor
 from torchvision import transforms
 
 class CustomDataset(Dataset):
-    def __init__(self,image_list,embeds_list,text_list,posterior_list,prompt_list):
+    def __init__(self,image_list,embeds_list,text_list,posterior_list,prompt_list=None):
         super().__init__()
         self.image_list=image_list
         self.embeds_list=embeds_list
@@ -17,10 +17,18 @@ class CustomDataset(Dataset):
         return len(self.image_list)
     
     def __getitem__(self, index):
-        return {
-            "image":self.image_list[index],
-            "embeds":self.embeds_list[index],
-            "text":self.text_list[index],
-            "posterior":self.posterior_list[index],
-            "prompt":self.prompt_list[index]
-        }
+        if self.prompt_list is not None:
+            return {
+                "image":self.image_list[index],
+                "embeds":self.embeds_list[index],
+                "text":self.text_list[index],
+                "posterior":self.posterior_list[index],
+                "prompt":self.prompt_list[index]
+            }
+        else:
+            return {
+                "image":self.image_list[index],
+                "embeds":self.embeds_list[index],
+                "text":self.text_list[index],
+                "posterior":self.posterior_list[index]
+            }

3cc22a8348a00df5fdb4e9c89f788b98c56882a3
James Baker <jlbaker361@gmail.com>
2025-05-30 09:20:09 -0400

    multi gpu

--- scripts/gen_test_deep.py
+++ scripts/gen_test_deep.py
@@ -1,11 +1,14 @@
+port=29501
+
 for training_type in ["denoise","reward"]:
     for prediction_type in ["epsilon","v_prediction"]:
         for embedding in ["dino","ssl","siglip2","clip"]:
             if prediction_type=="epsilon" and training_type=="reward":
                 continue
             name=f"{training_type}_{prediction_type}_{embedding}"
+            port+=1
             command=f"sbatch  -J pers  --err=slurm/test_deep/{name}.err --out=slurm/test_deep/{name}.out"
-            command+=" runaccgpu.sh --mixed_precision fp16 --num_processes 2 main_pers.py --epochs 2 --limit 20 --batch_size 4 --project_name testing-pers "
+            command+=f" runaccgpu.sh --multi_gpu --mixed_precision fp16 --num_processes 2 --main_process_port {port} main_pers.py --epochs 2 --limit 20 --batch_size 4 --project_name testing-pers "
             command+=f" --mixed_precision fp16 --prediction_type {prediction_type} "
             command+=f" --embedding {embedding} --training_type {training_type} --dataset jlbaker361/{embedding}-art_coco_captioned --vanilla --name jlbaker361/{name} "
             print(command)

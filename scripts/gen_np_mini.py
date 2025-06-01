df787a0817cb0cf8c334e846435c0485a9f819d6
James Baker <jlbaker361@gmail.com>
2025-06-01 19:39:00 -0400

    separate np

--- scripts/gen_np_mini.py
+++ scripts/gen_np_mini.py
@@ -1,6 +1,6 @@
 for embedding in ["dino","ssl","siglip2","clip"]:
     for data in ["league_captioned_tile","league_captioned_splash","coco_captioned","art_coco_captioned","celeb_captioned"]:
         name=f"{embedding}-{data}"
-        command=f"sbatch -J npz --err=slurm/npz/{name}.err --out=slurm/npz/{name}.out runpygpu.sh make_np_dataset.py  --embedding {embedding} "
+        command=f"sbatch -J npz --err=slurm/npz_mini/{name}.err --out=slurm/npz_mini/{name}.out runpygpu.sh make_np_dataset.py  --embedding {embedding} "
         command+=f" --dataset jlbaker361/{data} --output_dataset jlbaker361/{name}-20 --limit 20 --rewrite "
         print(command)

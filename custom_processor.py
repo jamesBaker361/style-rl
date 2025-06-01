b4b081b40c4ff31498044e20844a2b5ac8bf76ee
James Baker <jlbaker361@gmail.com>
2025-06-01 18:32:50 -0400

    defaults changed

--- custom_processor.py
+++ custom_processor.py
@@ -7,7 +7,7 @@ from torchvision.transforms import Normalize
 class CustomProcessor:
     def __init__(self,size:int=224,
                  rescale_factor:float=1.0, #0.00392156862745098,
-                 image_mean:list=[0.5,0.5,0.5],image_std:list=[0.5,0.5,0.5]):
+                 image_mean:list=[0.485,0.456,0.406],image_std:list=[0.229,0.224,0.225]):
         self.size=size
         self.rescale_factor=rescale_factor
         self.image_mean=image_mean
@@ -16,6 +16,7 @@ class CustomProcessor:
     def __call__(self, images:torch.Tensor)->dict:
         
         #resize
+        images=(images+1)/2 #convert from [-1,1] to 0,1
         images=F_v2.resize(images, (self.size,self.size))
         #rescale
         images=self.rescale_factor*images

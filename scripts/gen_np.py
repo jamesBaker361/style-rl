46d4d495aaf8bc3cba67f50723bff017447767fe
James Baker <jlbaker361@gmail.com>
2025-06-01 18:45:52 -0400

    kms

--- scripts/gen_np.py
+++ scripts/gen_np.py
@@ -1,4 +1,4 @@
-for embedding in ["clip"]:
+for embedding in ["clip","ssl","siglip2","dino"]:
     for data in ["league_captioned_tile","league_captioned_splash","coco_captioned","art_coco_captioned","celeb_captioned"]:
         name=f"{embedding}-{data}"
         command=f"sbatch -J npz --err=slurm/npz/{name}.err --out=slurm/npz/{name}.out runpygpu.sh make_np_dataset.py  --embedding {embedding} "

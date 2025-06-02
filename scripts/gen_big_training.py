34b535406f039ba3823466572b92474a2d130919
James Baker <jlbaker361@gmail.com>
2025-06-02 15:58:06 -0400

    conditiong

--- scripts/gen_big_training.py
+++ scripts/gen_big_training.py
@@ -1,13 +1,14 @@
 port=29601
 
 for training_type in  ["denoise","reward"]:
-    for prediction_type in ["epsilon"]:
-        for embedding in ["dino","ssl","clip","siglip2"]:
-            for data in ["league_captioned_tile"]:
-                name=f"{training_type}_{prediction_type}_{embedding}"
-                port+=1
-                command=f"sbatch  -J pers  --err=slurm/pers_{data}/{name}.err --out=slurm/pers_{data}/{name}.out"
-                command+=f" runaccgpu.sh --multi_gpu --mixed_precision fp16 --num_processes 2 --main_process_port {port} main_pers.py --epochs 100 --limit -1 --batch_size 4 --project_name {data}-20 "
-                command+=f" --mixed_precision fp16 --prediction_type {prediction_type} --upload_interval 10 "
-                command+=f" --embedding {embedding} --training_type {training_type} --dataset jlbaker361/{embedding}-{data}-20 --vanilla --name jlbaker361/{name} --gradient_accumulation_steps 8 "
-                print(command)
+    for frac in [0.0,0.5,1.0]:
+        for prediction_type in ["epsilon"]:
+            for embedding in ["clip"]:
+                for data in ["league_captioned_tile"]:
+                    name=f"{training_type}_{prediction_type}_{embedding}_{frac}"
+                    port+=1
+                    command=f"sbatch  -J pers  --err=slurm/pers_{data}/{name}.err --out=slurm/pers_{data}/{name}.out"
+                    command+=f" runaccgpu.sh --multi_gpu --mixed_precision fp16 --num_processes 2 --main_process_port {port} main_pers.py --epochs 1000 --limit -1 --batch_size 2 --project_name {data}-20 "
+                    command+=f" --mixed_precision fp16 --prediction_type {prediction_type} --upload_interval 100 "
+                    command+=f" --embedding {embedding} --training_type {training_type} --dataset jlbaker361/{embedding}-{data}-50 --vanilla --name jlbaker361/{name} --gradient_accumulation_steps 4 "
+                    print(command)

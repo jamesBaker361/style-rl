53b94ebec4c34163b7f0f5633a9dc64cc17bd82c
James Baker <jlbaker361@gmail.com>
2025-06-02 12:37:16 -0400

    images

--- img_test.py
+++ img_test.py
@@ -4,6 +4,7 @@ from diffusers import DiffusionPipeline
 import torch
 
 pipe = DiffusionPipeline.from_pretrained("SimianLuo/LCM_Dreamshaper_v7")
+pipe.load_ip_adapter("h94/IP-Adapter", subfolder="models", weight_name="ip-adapter_sd15.bin")
 
 def concat_images_horizontally(images):
     """
@@ -52,5 +53,10 @@ image=row["image"]
 processed=pipe.image_processor.preprocess(image)
 reconstructed=pipe.image_processor.postprocess(processed)[0]
 
-concat=concat_images_horizontally([image,reconstructed])
+img_20=pipe("woman",ip_adapter_image=image,num_inference_steps=20,height=256,width=256).images[0]
+img_10=pipe("woman",ip_adapter_image=image,num_inference_steps=10,height=256,width=256).images[0]
+img_4=pipe("woman",ip_adapter_image=image,num_inference_steps=4,height=256,width=256).images[0]
+img_2=pipe("woman",ip_adapter_image=image,num_inference_steps=2,height=256,width=256).images[0]
+
+concat=concat_images_horizontally([image,reconstructed,img_20,img_10,img_4,img_2])
 concat.save("comparison.jpg")

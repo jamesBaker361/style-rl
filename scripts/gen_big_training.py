f377760032049dac30cc0de0da2156b318cd0613
James Baker <jlbaker361@gmail.com>
2025-06-03 13:39:29 -0400

    more epochs

--- scripts/gen_big_training.py
+++ scripts/gen_big_training.py
@@ -1,6 +1,6 @@
-port=29601
+port=29650
 
-for training_type in  ["denoise","reward","latents_reward"]:
+for training_type in  ["denoise"]: #,"reward","latents_reward"]:
     for frac in [0.0,0.5,1.0]:
         for prediction_type in ["epsilon"]:
             for embedding in ["clip"]:
@@ -8,7 +8,7 @@ for training_type in  ["denoise","reward","latents_reward"]:
                     name=f"{training_type}_{prediction_type}_{embedding}_{frac}"
                     port+=1
                     command=f"sbatch  -J pers  --err=slurm/pers_{data}/{name}.err --out=slurm/pers_{data}/{name}.out"
-                    command+=f" runaccgpu.sh --multi_gpu --mixed_precision fp16 --num_processes 2 --main_process_port {port} main_pers.py --epochs 1000 --limit -1 --batch_size 2 --project_name {data}-50 "
+                    command+=f" runaccgpu.sh --multi_gpu --mixed_precision fp16 --num_processes 2 --main_process_port {port} main_pers.py --epochs 3000 --limit -1 --batch_size 2 --project_name {data}-50 "
                     command+=f" --mixed_precision fp16 --prediction_type {prediction_type} --upload_interval 100 "
-                    command+=f" --embedding {embedding} --training_type {training_type} --dataset jlbaker361/{embedding}-{data}-50 --vanilla --name jlbaker361/{name} --gradient_accumulation_steps 4 "
+                    command+=f" --embedding {embedding} --training_type {training_type} --dataset jlbaker361/{embedding}-{data}-50 --vanilla --name jlbaker361/{name} --gradient_accumulation_steps 4 --load "
                     print(command)

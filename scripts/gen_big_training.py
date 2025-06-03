694a29247ce5d19b2e76edbfb0bb355a229b813c
James Baker <jlbaker361@gmail.com>
2025-06-03 15:06:21 -0400

    frac

--- scripts/gen_big_training.py
+++ scripts/gen_big_training.py
@@ -1,14 +1,14 @@
 port=29650
 
 for training_type in  ["mse_reward","denoise"]: #,"reward","latents_reward"]:
-    for frac in [1.0]:
+    for frac in [1.0,0.0]:
         for prediction_type in ["epsilon"]:
             for embedding in ["clip"]:
                 for data in ["league_captioned_splash"]:
                     name=f"{training_type}_{prediction_type}_{embedding}_{frac}"
                     port+=1
                     command=f"sbatch  -J pers  --err=slurm/pers_{data}/{name}.err --out=slurm/pers_{data}/{name}.out"
-                    command+=f" runaccgpu.sh --multi_gpu --mixed_precision fp16 --num_processes 2 --main_process_port {port} main_pers.py --epochs 8000 --limit -1 --batch_size 2 --project_name {data}-50-{frac} "
+                    command+=f" runaccgpu.sh --multi_gpu --mixed_precision fp16 --num_processes 2 --main_process_port {port} main_pers.py --epochs 8000 --limit -1 --batch_size 2 --project_name {data}-50 "
                     command+=f" --mixed_precision fp16 --prediction_type {prediction_type} --upload_interval 100 --uncaptioned_frac {frac} "
                     command+=f" --embedding {embedding} --training_type {training_type} --dataset jlbaker361/{embedding}-{data}-50 --vanilla --name jlbaker361/{name} --gradient_accumulation_steps 4 --load "
                     print(command)
--- scripts/gen_big_training_chip.py
+++ scripts/gen_big_training_chip.py
@@ -4,11 +4,11 @@ for training_type in  ["denoise","mse_reward"]:
     for prediction_type in ["epsilon"]:
         for embedding in ["clip"]:
             for data in ["art_coco_captioned"]:
-                frac=1.0
-                name=f"{training_type}_{prediction_type}_{embedding}"
-                port+=1
-                command=f"sbatch  --gres=gpu:1  -J pers  --err=slurm_chip/pers_{data}/{name}.err --out=slurm_chip/pers_{data}/{name}.out"
-                command+=f" runaccgpu_chip.sh  --mixed_precision fp16 --num_processes 1 --main_process_port {port} main_pers.py --epochs 4000 --limit -1 --batch_size 2 --project_name {data}-50-{frac} "
-                command+=f" --mixed_precision fp16 --prediction_type {prediction_type} --upload_interval 100 --uncaptioned_frac {frac} "
-                command+=f" --embedding {embedding} --training_type {training_type} --dataset jlbaker361/{embedding}-{data}-50 --vanilla --name jlbaker361/{name} --gradient_accumulation_steps 8 "
-                print(command)
+                for frac in [1,0]:
+                    name=f"{training_type}_{prediction_type}_{embedding}_{frac}"
+                    port+=1
+                    command=f"sbatch  --gres=gpu:1  -J pers  --err=slurm_chip/pers_{data}/{name}.err --out=slurm_chip/pers_{data}/{name}.out"
+                    command+=f" runaccgpu_chip.sh  --mixed_precision fp16 --num_processes 1 --main_process_port {port} main_pers.py --epochs 4000 --limit -1 --batch_size 2 --project_name {data}-50 "
+                    command+=f" --mixed_precision fp16 --prediction_type {prediction_type} --upload_interval 100 --uncaptioned_frac {frac} "
+                    command+=f" --embedding {embedding} --training_type {training_type} --dataset jlbaker361/{embedding}-{data}-50 --vanilla --name jlbaker361/{name} --gradient_accumulation_steps 8 "
+                    print(command)

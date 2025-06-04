7faec86d499eae1bb49a169b43fd2966b5dfb01b
James Baker <jlbaker361@gmail.com>
2025-06-04 08:16:42 -0400

    500

--- scripts/gen_np_mini.py
+++ scripts/gen_np_mini.py
@@ -2,5 +2,5 @@ for embedding in ["siglip2","ssl","clip","dino"]:
     for data in ["league_captioned_tile","league_captioned_splash","coco_captioned","art_coco_captioned","celeb_captioned"]:
         name=f"{embedding}-{data}"
         command=f"sbatch -J npz --err=slurm/npz_mini/{name}.err --out=slurm/npz_mini/{name}.out runpygpu.sh make_np_dataset.py  --embedding {embedding} "
-        command+=f" --dataset jlbaker361/{data} --output_dataset jlbaker361/{name}-50 --limit 50 --rewrite "
+        command+=f" --dataset jlbaker361/{data} --output_dataset jlbaker361/{name}-500 --limit 500 --rewrite "
         print(command)

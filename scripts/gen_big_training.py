d250abd4b5c235f7aa4e871d3733ca1fdb59af01
James Baker <jlbaker361@gmail.com>
2025-06-05 14:09:46 -0400

    identity adn no projection

--- scripts/gen_big_training.py
+++ scripts/gen_big_training.py
@@ -6,11 +6,16 @@ for training_type in  ["denoise"]: #,"reward","latents_reward"]:
             for embedding in ["clip"]:
                 for data in ["league_captioned_splash"]:
                     for lr in [0.001,0.0001]:
-                        n=1000
-                        name=f"{training_type}_{prediction_type}_{embedding}_{frac}_{lr}_{n}"
-                        port+=1
-                        command=f"sbatch  -J pers  --err=slurm/pers_{data}/{name}.err --out=slurm/pers_{data}/{name}.out"
-                        command+=f" runaccgpu.sh --multi_gpu --mixed_precision fp16 --num_processes 2 --main_process_port {port} main_pers.py --epochs 400 --limit -1 --batch_size 2 --project_name {data}-{n} "
-                        command+=f" --mixed_precision fp16 --prediction_type {prediction_type} --upload_interval 100 --uncaptioned_frac {frac} --train_split 0.95 --lr {lr} --load --generic_test_prompts "
-                        command+=f" --embedding {embedding} --training_type {training_type} --dataset jlbaker361/{embedding}-{data}-{n} --vanilla --name jlbaker361/{name} --gradient_accumulation_steps 4  "
-                        print(command)
+                        for n in [500,1000]:
+                            for suffix in ["_no_proj","_identity"]:
+                                name=f"{training_type}_{prediction_type}_{embedding}_{frac}_{lr}_{n}{suffix}"
+                                port+=1
+                                command=f"sbatch  -J pers  --err=slurm/pers_{data}/{name}.err --out=slurm/pers_{data}/{name}.out"
+                                command+=f" runaccgpu.sh --multi_gpu --mixed_precision fp16 --num_processes 2 --main_process_port {port} main_pers.py --epochs 400 --limit -1 --batch_size 2 --project_name {data}-{n} "
+                                command+=f" --mixed_precision fp16 --prediction_type {prediction_type} --upload_interval 100 --uncaptioned_frac {frac} --train_split 0.95 --lr {lr} --load --generic_test_prompts "
+                                command+=f" --embedding {embedding} --training_type {training_type} --dataset jlbaker361/{embedding}-{data}-{n} --vanilla --name jlbaker361/{name} --gradient_accumulation_steps 4  "
+                                if suffix=="_no_proj":
+                                    command+=" --disable_projection_adapter "
+                                elif suffix=="_identity":
+                                    command+=" --identity_adapter "
+                                print(command)
--- scripts/gen_big_training_chip.py
+++ scripts/gen_big_training_chip.py
@@ -6,11 +6,16 @@ for training_type in  ["denoise"]:
             for data in ["art_coco_captioned"]:
                 for frac in [1]:
                     for lr in [0.001,0.0001]:
-                        n=1000
-                        name=f"{training_type}_{prediction_type}_{embedding}_{frac}_{lr}_{n}"
-                        port+=1
-                        command=f"sbatch  --gres=gpu:1  -J pers  --err=slurm_chip/pers_{data}/{name}.err --out=slurm_chip/pers_{data}/{name}.out"
-                        command+=f" runaccgpu_chip.sh  --mixed_precision fp16 --num_processes 1 --main_process_port {port} main_pers.py --epochs 5000 --limit -1 --batch_size 2 --project_name {data}-{n} "
-                        command+=f" --mixed_precision fp16 --prediction_type {prediction_type} --upload_interval 100 --uncaptioned_frac {frac} --train_split 0.8 --lr {lr} --load  --generic_test_prompts "
-                        command+=f" --embedding {embedding} --training_type {training_type} --dataset jlbaker361/{embedding}-{data}-{n} --vanilla --name jlbaker361/{name} --gradient_accumulation_steps 8 "
-                        print(command)
+                        for suffix in ["_no_proj","_identity"]:
+                            for n in [500,1000]:
+                                name=f"{training_type}_{prediction_type}_{embedding}_{frac}_{lr}_{n}{suffix}"
+                                port+=1
+                                command=f"sbatch  --gres=gpu:1  -J pers  --err=slurm_chip/pers_{data}/{name}.err --out=slurm_chip/pers_{data}/{name}.out"
+                                command+=f" runaccgpu_chip.sh  --mixed_precision fp16 --num_processes 1 --main_process_port {port} main_pers.py --epochs 5000 --limit -1 --batch_size 2 --project_name {data}-{n} "
+                                command+=f" --mixed_precision fp16 --prediction_type {prediction_type} --upload_interval 100 --uncaptioned_frac {frac} --train_split 0.8 --lr {lr} --load  --generic_test_prompts "
+                                command+=f" --embedding {embedding} --training_type {training_type} --dataset jlbaker361/{embedding}-{data}-{n} --vanilla --name jlbaker361/{name} --gradient_accumulation_steps 8 "
+                                if suffix=="_no_proj":
+                                    command+=" --disable_projection_adapter "
+                                elif suffix=="_identity":
+                                    command+=" --identity_adapter "
+                                print(command)

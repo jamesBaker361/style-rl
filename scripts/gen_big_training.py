70e4ba55ce628571376dbc8a1907613861e249d2
James Baker <jlbaker361@gmail.com>
2025-06-05 10:57:15 -0400

    --

--- scripts/gen_big_training.py
+++ scripts/gen_big_training.py
@@ -10,6 +10,6 @@ for training_type in  ["reward","denoise"]: #,"reward","latents_reward"]:
                         port+=1
                         command=f"sbatch  -J pers  --err=slurm/pers_{data}/{name}.err --out=slurm/pers_{data}/{name}.out"
                         command+=f" runaccgpu.sh --multi_gpu --mixed_precision fp16 --num_processes 2 --main_process_port {port} main_pers.py --epochs 5000 --limit -1 --batch_size 2 --project_name {data}-50 "
-                        command+=f" --mixed_precision fp16 --prediction_type {prediction_type} --upload_interval 100 --uncaptioned_frac {frac} --train_split 0.8 --lr {lr} --load generic_test_prompts "
+                        command+=f" --mixed_precision fp16 --prediction_type {prediction_type} --upload_interval 100 --uncaptioned_frac {frac} --train_split 0.8 --lr {lr} --load --generic_test_prompts "
                         command+=f" --embedding {embedding} --training_type {training_type} --dataset jlbaker361/{embedding}-{data}-50 --vanilla --name jlbaker361/{name} --gradient_accumulation_steps 4  "
                         print(command)

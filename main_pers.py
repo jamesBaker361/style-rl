0a5ec7f07488b4287724c0425d81dbd1c6b5417b
James Baker <jlbaker361@gmail.com>
2025-06-08 09:56:10 -0400

    inference steps

--- main_pers.py
+++ main_pers.py
@@ -514,9 +514,9 @@ def main(args):
             #image_batch=torch.clamp(image_batch, 0, 1)
             if baseline:
                 ip_adapter_image=F_v2.resize(image_batch, (224,224))
-                fake_image=pipeline(prompt_embeds=text_batch,ip_adapter_image=ip_adapter_image,output_type="pt",height=args.image_size,width=args.image_size).images
+                fake_image=pipeline( num_inference_steps=args.num_inference_steps,prompt_embeds=text_batch,ip_adapter_image=ip_adapter_image,output_type="pt",height=args.image_size,width=args.image_size).images
             else:
-                fake_image=pipeline(prompt_embeds=text_batch,ip_adapter_image_embeds=[image_embeds],output_type="pt",height=args.image_size,width=args.image_size).images
+                fake_image=pipeline(num_inference_steps=args.num_inference_steps,prompt_embeds=text_batch,ip_adapter_image_embeds=[image_embeds],output_type="pt",height=args.image_size,width=args.image_size).images
             #normal_image_set=pipeline(prompt_embeds=text_batch,output_type="pil").images
             image_batch=F_v2.resize(image_batch, (args.image_size,args.image_size))
             #print("img vs real img",fake_image.size(),image_batch.size())

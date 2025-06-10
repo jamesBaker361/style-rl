781dbb7902f09d64433f36a52c1fc6eb8c8c7d18
James Baker <jlbaker361@gmail.com>
2025-06-10 19:28:10 -0400

    printign

--- sana_pipelines.py
+++ sana_pipelines.py
@@ -74,6 +74,7 @@ def recursively_prepare_ip_adapter(model:torch.nn.Module,
     if type(model)!=torch.nn.Module:
         return
     if hasattr(model,"attn2"):
+        print("has it???")
         model.attn2=set_ip_adapter_attn(model.attn2,
             qk_norm,
             num_cross_attention_heads,

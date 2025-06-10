a683a5ad835eaf80cd4fa168724c863c95b802c8
James Baker <jlbaker361@gmail.com>
2025-06-09 21:16:48 -0400

    ???

--- main_pers.py
+++ main_pers.py
@@ -516,6 +516,7 @@ def main(args):
         for b,batch in enumerate(data_loader):
             if args.vanilla:
                 pipeline.vae=pipeline.vae.to(device)
+                pipeline.text_encoder=pipeline.text_encoder.to(device)
                 for k,v in batch.items():
                     if type(v)==torch.Tensor:
                         batch[k]=v.to(pipeline.unet.device,torch_dtype)

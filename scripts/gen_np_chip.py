ccc184c229c742aeb46f321fe9eafcaac7b2313f
James Baker <jlbaker361@gmail.com>
2025-06-11 16:49:37 -0400

    gen np sana

--- scripts/gen_np_chip.py
+++ scripts/gen_np_chip.py
@@ -3,6 +3,6 @@ for embedding in ["siglip2","ssl","clip","dino"]:
         for pipeline in ["sana"]:
             for limit in [1000,-1]:
                 name=f"{embedding}-{data}-{limit}-{pipeline}"
-                command=f"sbatch -J npz --err=slurm/npz_mini/{name}.err --out=slurm/npz_mini/{name}.out runpygpu.sh make_np_dataset.py  --embedding {embedding} "
+                command=f"sbatch -J npz --err=slurm_chip/npz/{name}.err --out=slurm_chip/npz/{name}.out runpygpu_chip.sh make_np_dataset.py  --embedding {embedding} "
                 command+=f" --dataset jlbaker361/{data} --output_dataset jlbaker361/{name}-{limit} --limit {limit} --rewrite --pipeline {pipeline} "
                 print(command)

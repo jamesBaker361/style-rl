dcc13f3fd8262a8336965e03909301085dd2afb3
James Baker <jlbaker361@gmail.com>
2025-06-12 13:25:35 -0400

    extra limit

--- scripts/gen_np_chip.py
+++ scripts/gen_np_chip.py
@@ -4,5 +4,5 @@ for embedding in ["siglip2","ssl","clip","dino"]:
             for limit in [1000,-1]:
                 name=f"{embedding}-{data}-{limit}-{pipeline}"
                 command=f"sbatch -J npz --err=slurm_chip/npz/{name}.err --out=slurm_chip/npz/{name}.out runpygpu_chip.sh make_np_dataset.py  --embedding {embedding} "
-                command+=f" --dataset jlbaker361/{data} --output_dataset jlbaker361/{name}-{limit} --limit {limit} --rewrite --pipeline {pipeline} "
+                command+=f" --dataset jlbaker361/{data} --output_dataset jlbaker361/{name} --limit {limit} --rewrite --pipeline {pipeline} "
                 print(command)

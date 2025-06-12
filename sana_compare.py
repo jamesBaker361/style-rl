6529da9866b91a4b36d8a66fc2a6228a08396755
James Baker <jlbaker361@gmail.com>
2025-06-12 02:09:26 -0400

    casting

--- sana_compare.py
+++ sana_compare.py
@@ -14,7 +14,7 @@ image = load_image("https://huggingface.co/datasets/huggingface/documentation-im
 
 parser=argparse.ArgumentParser()
 
-parser.add_argument("--mixed_precision",type=str,default="no")
+parser.add_argument("--mixed_precision",type=str,default="fp16")
 parser.add_argument("--project_name",type=str,default="compare")
 
 
@@ -22,66 +22,67 @@ parser.add_argument("--project_name",type=str,default="compare")
 def main(args):
     accelerator=Accelerator(log_with="wandb",mixed_precision=args.mixed_precision)
     accelerator.init_trackers(project_name=args.project_name,config=vars(args))
-    generator=torch.Generator(accelerator.device)
-    generator.manual_seed(123)
+    with accelerator.autocast():
+        generator=torch.Generator(accelerator.device)
+        generator.manual_seed(123)
 
-    pipeline = SanaSprintPipeline.from_pretrained(
-    "Efficient-Large-Model/Sana_Sprint_0.6B_1024px_diffusers",
-    torch_dtype=torch.bfloat16
-    )
-    pipeline.enable_vae_tiling()
-    pipeline.to(accelerator.device)
+        pipeline = SanaSprintPipeline.from_pretrained(
+        "Efficient-Large-Model/Sana_Sprint_0.6B_1024px_diffusers",
+        torch_dtype=torch.float16
+        )
+        pipeline.enable_vae_tiling()
+        pipeline.to(accelerator.device)
 
-    prompt = "a tiny astronaut hatching from an egg on the moon"
+        prompt = "a tiny astronaut hatching from an egg on the moon"
 
-    image0 = pipeline(prompt=prompt, num_inference_steps=2,generator=generator,height=256,width=256).images[0]
-    config=pipeline.transformer.config
+        image0 = pipeline(prompt=prompt, num_inference_steps=2,generator=generator,height=256,width=256).images[0]
+        config=pipeline.transformer.config
 
-    generator=torch.Generator(accelerator.device)
-    generator.manual_seed(123)
+        generator=torch.Generator(accelerator.device)
+        generator.manual_seed(123)
 
-    pipeline = CompatibleSanaSprintPipeline.from_pretrained(
-    "Efficient-Large-Model/Sana_Sprint_0.6B_1024px_diffusers",
-    torch_dtype=torch.bfloat16
-    )
-    pipeline.enable_vae_tiling()
-    pipeline.to(accelerator.device)
+        pipeline = CompatibleSanaSprintPipeline.from_pretrained(
+        "Efficient-Large-Model/Sana_Sprint_0.6B_1024px_diffusers",
+        torch_dtype=torch.float16
+        )
+        pipeline.enable_vae_tiling()
+        pipeline.to(accelerator.device)
 
-    prompt = "a tiny astronaut hatching from an egg on the moon"
+        prompt = "a tiny astronaut hatching from an egg on the moon"
 
-    image1 = pipeline(prompt=prompt, num_inference_steps=2,generator=generator,height=256,width=256).images[0]
-    ip_cross_attention_dim=256
-    embedding_dim=512
-    
-    print("pipleine params",len([p for  name,p in pipeline.transformer.named_parameters()]))
+        image1 = pipeline(prompt=prompt, num_inference_steps=2,generator=generator,height=256,width=256).images[0]
+        ip_cross_attention_dim=256
+        embedding_dim=512
+        
+        print("pipleine params",len([p for  name,p in pipeline.transformer.named_parameters()]))
 
-    prepare_ip_adapter(pipeline.transformer,accelerator.device,torch.bfloat16,ip_cross_attention_dim)
-    print("pipleine params",len([p for  name,p in pipeline.transformer.named_parameters()]))
-    encoder_hid_proj=replace_ip_attn(pipeline.transformer,embedding_dim,512,ip_cross_attention_dim,4,True,return_encoder_hid_proj=True)
-    print("pipleine params",len([p for  name,p in pipeline.transformer.named_parameters()]))
-    pipeline.set_encoder_hid_proj(encoder_hid_proj)
-    print("pipleine params",len([p for  name,p in pipeline.transformer.named_parameters()]))
-    '''for block in pipeline.transformer.transformer_blocks:
-        print(block.attn2.processor)'''
-    #embeds.shape = [N_a,B,N_i,D] N_a= # of adapters, N_i = images per image prompt, D =dimension of embedding
-    image1 = pipeline(prompt=prompt, num_inference_steps=2,generator=generator,height=256,width=256,ip_adapter_image_embeds=[torch.zeros((1,1,embedding_dim),device=accelerator.device,dtype=torch.bfloat16)]).images[0]
+        prepare_ip_adapter(pipeline.transformer,accelerator.device,torch.float16,ip_cross_attention_dim)
+        print("pipleine params",len([p for  name,p in pipeline.transformer.named_parameters()]))
+        encoder_hid_proj=replace_ip_attn(pipeline.transformer,embedding_dim,512,ip_cross_attention_dim,4,True,return_encoder_hid_proj=True)
+        print("pipleine params",len([p for  name,p in pipeline.transformer.named_parameters()]))
+        pipeline.set_encoder_hid_proj(encoder_hid_proj)
+        print("pipleine params",len([p for  name,p in pipeline.transformer.named_parameters()]))
+        '''for block in pipeline.transformer.transformer_blocks:
+            print(block.attn2.processor)'''
+        #embeds.shape = [N_a,B,N_i,D] N_a= # of adapters, N_i = images per image prompt, D =dimension of embedding
+        image1 = pipeline(prompt=prompt, num_inference_steps=2,generator=generator,height=256,width=256,ip_adapter_image_embeds=[torch.zeros((1,1,embedding_dim),device=accelerator.device,dtype=torch.float16)]).images[0]
 
-    
+        
 
-    #pipeline.transformer=replace_ip_attn(pipeline.transformer,ip_cross_attention_dim,512,ip_cross_attention_dim,4,True)
-    #image2 = pipeline(prompt=prompt, num_inference_steps=2,generator=generator,height=256,width=256,ip_adapter_image_embeds=torch.zeros((1,1,ip_cross_attention_dim),device=accelerator.device,dtype=torch.bfloat16)).images[0]
+        #pipeline.transformer=replace_ip_attn(pipeline.transformer,ip_cross_attention_dim,512,ip_cross_attention_dim,4,True)
+        #image2 = pipeline(prompt=prompt, num_inference_steps=2,generator=generator,height=256,width=256,ip_adapter_image_embeds=torch.zeros((1,1,ip_cross_attention_dim),device=accelerator.device,dtype=torch.bfloat16)).images[0]
 
-    encoder_hid_proj=replace_ip_attn(pipeline.transformer,ip_cross_attention_dim,512,ip_cross_attention_dim,4,True,deep_to_ip_layers=True,return_encoder_hid_proj=True)
-    pipeline.set_encoder_hid_proj(encoder_hid_proj)
-    
-    image3 = pipeline(prompt=prompt, num_inference_steps=2,generator=generator,height=256,width=256,ip_adapter_image_embeds=[torch.zeros((1,1,ip_cross_attention_dim),device=accelerator.device,dtype=torch.bfloat16)]).images[0]
+        encoder_hid_proj=replace_ip_attn(pipeline.transformer,ip_cross_attention_dim,512,ip_cross_attention_dim,4,True,deep_to_ip_layers=True,return_encoder_hid_proj=True)
+        pipeline.set_encoder_hid_proj(encoder_hid_proj)
+        
+        image3 = pipeline(prompt=prompt, num_inference_steps=2,generator=generator,height=256,width=256,ip_adapter_image_embeds=[torch.zeros((1,1,ip_cross_attention_dim),device=accelerator.device,dtype=torch.float16)]).images[0]
 
-    accelerator.log({
-        "image1":wandb.Image(image1),
-        "image0":wandb.Image(image0),
-       # "image2":wandb.Image(image2),
-        "image3":wandb.Image(image3),
-    })    
+        accelerator.log({
+            "image1":wandb.Image(image1),
+            "image0":wandb.Image(image0),
+        # "image2":wandb.Image(image2),
+            "image3":wandb.Image(image3),
+        })    
 
 
 

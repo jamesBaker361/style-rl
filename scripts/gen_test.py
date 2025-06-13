9bbdd14759930bb5e627cbc76efdc1cde32fcc3e
James Baker <jlbaker361@gmail.com>
2025-06-13 11:08:03 -0400

    50

--- scripts/gen_test.py
+++ scripts/gen_test.py
@@ -8,5 +8,5 @@ for training_type in ["denoise","reward"]:
                 command=f"sbatch  -J pers --exclude=gpu[005,006,010-014,017,018],cuda[001-008],pascal[001-010],gpuk[001-012] --err=slurm/test/{name}.err --out=slurm/test/{name}.out"
                 command+=" runpygpu.sh main_pers.py --epochs 3 --limit 10 --project_name testing-pers --reward_switch_epoch 2 "
                 command+=f" --mixed_precision fp16 --prediction_type {prediction_type} "
-                command+=f" --embedding {embedding} --training_type {training_type} --dataset jlbaker361/{embedding}-art_coco_captioned-{pipeline}"
+                command+=f" --embedding {embedding} --training_type {training_type} --dataset jlbaker361/{embedding}-art_coco_captioned-50-{pipeline}"
                 print(command)

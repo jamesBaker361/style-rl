4086e31b9b2688a6b7587f0269a56fd66de69ec2
James Baker <jlbaker361@gmail.com>
2025-06-13 15:19:58 -0400

    sana test

--- scripts/gen_test.py
+++ scripts/gen_test.py
@@ -3,10 +3,12 @@ for training_type in ["denoise","reward"]:
         for embedding in ["clip"]:
             if prediction_type=="epsilon" and training_type=="reward":
                 continue
-            for pipeline in ["sana"]:
+            for pipeline in ["sana","lcm"]:
                 name=f"{training_type}_{prediction_type}_{embedding}_{pipeline}"
                 command=f"sbatch  -J pers --exclude=gpu[005,006,010-014,017,018],cuda[001-008],pascal[001-010],gpuk[001-012] --err=slurm/test/{name}.err --out=slurm/test/{name}.out"
                 command+=" runpygpu.sh main_pers.py --epochs 3 --limit 10 --project_name testing-pers --reward_switch_epoch 2 "
                 command+=f" --mixed_precision fp16 --prediction_type {prediction_type} --pipeline {pipeline} "
-                command+=f" --embedding {embedding} --training_type {training_type} --dataset jlbaker361/{embedding}-art_coco_captioned-50-{pipeline}"
+                command+=f" --embedding {embedding} --training_type {training_type} --dataset jlbaker361/{embedding}-art_coco_captioned-50"
+                if pipeline=="sana":
+                    command+=f"-{pipeline} "
                 print(command)

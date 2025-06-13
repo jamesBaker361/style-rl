f1bece428a9b2cef830d61a0846ee6dafdf9955e
James Baker <jlbaker361@gmail.com>
2025-06-13 09:00:41 -0400

    50

--- scripts/gen_np.py
+++ scripts/gen_np.py
@@ -1,7 +1,7 @@
 for embedding in ["siglip2","ssl","clip","dino"]:
     for data in ["league_captioned_splash","coco_captioned","art_coco_captioned"]:
         for pipeline in ["sana"]:
-            for limit in [1000,-1]:
+            for limit in [50]:
                 name=f"{embedding}-{data}-{limit}-{pipeline}"
                 command=f"sbatch -J npz --err=slurm/npz/{name}.err --out=slurm/npz/{name}.out runpygpu.sh make_np_dataset.py  --embedding {embedding} "
                 command+=f" --dataset jlbaker361/{data} --output_dataset jlbaker361/{name} --limit {limit}  --pipeline {pipeline} "

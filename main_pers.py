7dc67b86262dc5a9fa3115633a30912d08602ac5
James Baker <jlbaker361@gmail.com>
2025-06-13 16:29:57 -0400

    moved img helpers

--- main_pers.py
+++ main_pers.py
@@ -40,41 +40,7 @@ from huggingface_hub import create_repo,HfApi
 from PIL import Image
 from sana_pipelines import CompatibleSanaSprintPipeline, prepare_ip_adapter,compatible_forward_sana_transformer_model
 from custom_scheduler import CompatibleFlowMatchEulerDiscreteScheduler
-
-def concat_images_horizontally(images):
-    """
-    Concatenate a list of PIL.Image objects horizontally.
-
-    Args:
-        images (List[PIL.Image]): List of PIL images.
-
-    Returns:
-        PIL.Image: A new image composed of the input images concatenated side-by-side.
-    """
-    # Resize all images to the same height (optional)
-    heights = [img.height for img in images]
-    min_height = min(heights)
-    resized_images = [
-        img if img.height == min_height else img.resize(
-            (int(img.width * min_height / img.height), min_height),
-            Image.LANCZOS
-        ) for img in images
-    ]
-
-    # Compute total width and max height
-    total_width = sum(img.width for img in resized_images)
-    height = min_height
-
-    # Create new blank image
-    new_img = Image.new('RGB', (total_width, height))
-
-    # Paste images side by side
-    x_offset = 0
-    for img in resized_images:
-        new_img.paste(img, (x_offset, 0))
-        x_offset += img.width
-
-    return new_img
+from img_helpers import concat_images_horizontally
 
 
 seed=1234

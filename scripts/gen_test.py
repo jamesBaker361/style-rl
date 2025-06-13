0b928f788772d1d4929f2454752a621f2fa5a4b7
James Baker <jlbaker361@gmail.com>
2025-06-13 10:58:11 -0400

    sana test?

--- scripts/gen_test.py
+++ scripts/gen_test.py
@@ -1,11 +1,12 @@
 for training_type in ["denoise","reward"]:
     for prediction_type in ["epsilon","v_prediction"]:
-        for embedding in ["dino","ssl","siglip2","clip"]:
+        for embedding in ["clip"]:
             if prediction_type=="epsilon" and training_type=="reward":
                 continue
-            name=f"{training_type}_{prediction_type}_{embedding}"
-            command=f"sbatch  -J pers --exclude=gpu[005,006,010-014,017,018],cuda[001-008],pascal[001-010],gpuk[001-012] --err=slurm/test/{name}.err --out=slurm/test/{name}.out"
-            command+=" runpygpu.sh main_pers.py --epochs 3 --limit 10 --project_name testing-pers --reward_switch_epoch 2 "
-            command+=f" --mixed_precision fp16 --prediction_type {prediction_type} "
-            command+=f" --embedding {embedding} --training_type {training_type} --dataset jlbaker361/{embedding}-art_coco_captioned"
-            print(command)
+            for pipeline in ["sana"]:
+                name=f"{training_type}_{prediction_type}_{embedding}_{pipeline}"
+                command=f"sbatch  -J pers --exclude=gpu[005,006,010-014,017,018],cuda[001-008],pascal[001-010],gpuk[001-012] --err=slurm/test/{name}.err --out=slurm/test/{name}.out"
+                command+=" runpygpu.sh main_pers.py --epochs 3 --limit 10 --project_name testing-pers --reward_switch_epoch 2 "
+                command+=f" --mixed_precision fp16 --prediction_type {prediction_type} "
+                command+=f" --embedding {embedding} --training_type {training_type} --dataset jlbaker361/{embedding}-art_coco_captioned-{pipeline}"
+                print(command)

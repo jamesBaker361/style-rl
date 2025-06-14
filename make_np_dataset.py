ffac159369bac623a218eeb3e8b823c3eb37b271
James Baker <jlbaker361@gmail.com>
2025-06-14 00:19:10 -0400

    resizing

--- make_np_dataset.py
+++ make_np_dataset.py
@@ -124,7 +124,7 @@ def main(args):
                 if k==0:
                     for size in [256,512,1024]:
                         image=new_dataset["image"][-1]
-                        image=pipeline.image_processor.preprocess(image.resize(size,size))
+                        image=pipeline.image_processor.preprocess(image.resize((size,size)))
                         posterior=public_encode(pipeline.vae,image.to(accelerator.device,torch_dtype)).squeeze(0).cpu().detach().numpy()
                         print("image max min",image.max(),image.min())
                         print("post min max",public_encode(pipeline.vae,image.to(accelerator.device,torch_dtype)).max(),public_encode(pipeline.vae,image.to(accelerator.device,torch_dtype)).min())

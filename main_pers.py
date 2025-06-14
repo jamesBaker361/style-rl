11d06c24bc7d8e0fcb61099d3aff6c03a7d718e7
James Baker <jlbaker361@gmail.com>
2025-06-14 00:47:38 -0400

    conditional device placemtn

--- main_pers.py
+++ main_pers.py
@@ -497,8 +497,8 @@ def main(args):
 
     optimizer=torch.optim.AdamW(params,lr=args.lr)
 
-    #if args.vanilla:
-    #denoising_model=denoising_model.to(device)
+    if args.vanilla:
+        denoising_model=denoising_model.to(device)
 
     #if args.training_type=="reward":
     vae=vae.to(denoising_model.device)

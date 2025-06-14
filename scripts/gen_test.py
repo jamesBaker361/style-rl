d56e076b35bc31eaa1426b244e5c8179431b68ee
James Baker <jlbaker361@gmail.com>
2025-06-13 22:40:18 -0400

    2 steps

--- scripts/gen_test.py
+++ scripts/gen_test.py
@@ -7,7 +7,7 @@ for training_type in ["denoise","reward"]:
                 name=f"{training_type}_{prediction_type}_{embedding}_{pipeline}"
                 command=f"sbatch  -J pers --exclude=gpu[005,006,010-014,017,018],cuda[001-008],pascal[001-010],gpuk[001-012] --err=slurm/test/{name}.err --out=slurm/test/{name}.out"
                 command+=" runpygpu.sh main_pers.py --epochs 3 --limit 10 --project_name testing-pers --reward_switch_epoch 2 "
-                command+=f" --mixed_precision fp16 --prediction_type {prediction_type} --pipeline {pipeline} "
+                command+=f" --mixed_precision fp16 --prediction_type {prediction_type} --pipeline {pipeline} --num_inference_steps 2 "
                 command+=f" --embedding {embedding} --training_type {training_type} --dataset jlbaker361/{embedding}-art_coco_captioned-50"
                 if pipeline=="sana":
                     command+=f"-{pipeline} "

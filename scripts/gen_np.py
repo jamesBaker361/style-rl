f31e73d3f4287f41313237c58da35dbee2d20cb1
James Baker <jlbaker361@gmail.com>
2025-06-14 00:35:33 -0400

    precision

--- scripts/gen_np.py
+++ scripts/gen_np.py
@@ -4,5 +4,5 @@ for embedding in ["siglip2","ssl","clip","dino"]:
             for limit in [50]:
                 name=f"{embedding}-{data}-{limit}-{pipeline}"
                 command=f"sbatch -J npz --err=slurm/npz/{name}.err --out=slurm/npz/{name}.out runpygpu.sh make_np_dataset.py  --embedding {embedding} "
-                command+=f" --dataset jlbaker361/{data} --output_dataset jlbaker361/{name} --limit {limit}  --pipeline {pipeline} --rewrite "
+                command+=f" --dataset jlbaker361/{data} --output_dataset jlbaker361/{name} --limit {limit}  --pipeline {pipeline} --rewrite --mixed_precision no "
                 print(command)

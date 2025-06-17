f2f543d1c08b9f56e0af44e452abfe29af5150e2
James Baker <jlbaker361@gmail.com>
2025-06-17 14:28:10 -0400

    denrmalizing

--- custom_processor.py
+++ custom_processor.py
@@ -7,16 +7,24 @@ from torchvision.transforms import Normalize
 class CustomProcessor:
     def __init__(self,size:int=224,
                  rescale_factor:float=1.0, #0.00392156862745098,
-                 image_mean:list=[0.485,0.456,0.406],image_std:list=[0.229,0.224,0.225]):
+                 image_mean:list=[0.485,0.456,0.406],image_std:list=[0.229,0.224,0.225],
+                 denormalize:bool=False):
         self.size=size
         self.rescale_factor=rescale_factor
         self.image_mean=image_mean
         self.image_std=image_std
+        self.denormalize=denormalize
     
     def __call__(self, images:torch.Tensor)->dict:
         
         #resize
-        images=(images+1)/2 #convert from [-1,1] to 0,1
+        if self.denormalize:
+            images=(images+1)/2 #convert from [-1,1] to 0,1
+
+        if self.denormalize and images.min()>=0:
+            print(f"images are already denormalized max: {images.max()} min: {images.min()}")
+        if self.denormalize==False and images.min()<0:
+            print(f"images should be denormalized max: {images.max()} min: {images.min()}")
         images=F_v2.resize(images, (self.size,self.size))
         #rescale
         images=self.rescale_factor*images

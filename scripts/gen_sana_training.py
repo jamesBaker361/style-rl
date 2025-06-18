d3631c88f25e5fb6cfaf728a933657e5adcb3869
James Baker <jlbaker361@gmail.com>
2025-06-18 19:10:32 -0400

    gen training

--- scripts/gen_sana_training.py
+++ scripts/gen_sana_training.py
@@ -1,22 +1,22 @@
 port=29650
 
-for training_type in  ["denoise","reward"]: #,"reward","latents_reward"]:
+for training_type in  ["reward"]: #,"reward","latents_reward"]:
     for frac in [1.0]:
         for prediction_type in ["v_prediction"]:
             for embedding in ["clip",]:
-                for data in ["league_captioned_splash"]:
+                for data in ["art_coco_captioned"]:
                     for suffix in ["_deep_identity",""]:
                         for lr in [0.001,0.0001]:
                             for n in [1000]:
                                 #for scheduler in ["LCMScheduler"]:
-                                for pipeline in ["lcm","lcm_pre_lora","lcm_post_lora"]:
+                                for pipeline in ["sana"]:
                         
                                     name=f"{training_type}_{prediction_type}_{embedding}_{frac}_{lr}_{n}{suffix}_{pipeline}"
                                     port+=1
                                     command=f"sbatch  -J pers  --err=slurm/pers_{data}/{name}.err --out=slurm/pers_{data}/{name}.out --gres=gpu:1 "
                                     command+=f" runaccgpu.sh  --mixed_precision fp16 --num_processes 1 --main_process_port {port} main_pers.py --epochs 500 --limit -1 --batch_size 2 --project_name {data}-{n} "
                                     command+=f" --mixed_precision fp16 --prediction_type {prediction_type} --upload_interval 5 --uncaptioned_frac {frac} --train_split 0.95 --lr {lr} --load --generic_test_prompts "
-                                    command+=f" --embedding {embedding} --training_type {training_type} --dataset jlbaker361/{embedding}-{data}-{n} --vanilla --name jlbaker361/{name} --gradient_accumulation_steps 8  "
+                                    command+=f" --embedding {embedding} --training_type {training_type} --dataset jlbaker361/{embedding}-{data}-{n}-{pipeline} --vanilla --name jlbaker361/{name} --gradient_accumulation_steps 8  "
                                     if training_type=="reward":
                                         command+=" --initial_scale 0.25 "
                                     if suffix=="_no_proj":

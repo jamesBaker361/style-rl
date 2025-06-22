ac1d655d00e200c089734979c1d2e562ed7caba4
James Baker <jlbaker361@gmail.com>
2025-06-22 16:46:03 -0400

    image

--- main_pers.py
+++ main_pers.py
@@ -684,7 +684,7 @@ def main(args):
             clip_alignment_list.append(clip_difference.cpu().detach().item())
 
             for pil_image,real_pil_image,pil_image_unnorm,prompt in zip(pil_image_set,real_pil_image_set,pil_image_set_unnorm,prompt_batch):
-                concat_image=concat_images_horizontally([real_pil_image,pil_image_set_unnorm,pil_image])
+                concat_image=concat_images_horizontally([real_pil_image,pil_image_unnorm,pil_image])
                 metrics[prompt.replace(",","").replace(" ","_").strip()]=wandb.Image(concat_image)
         #pipeline.scheduler =  DEISMultistepScheduler.from_config(pipeline.scheduler.config)
         metrics["difference"]=np.mean(difference_list)

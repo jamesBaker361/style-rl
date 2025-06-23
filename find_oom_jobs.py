1bec2675824e064805346a19df88e0cdb19c6e83
James Baker <jlbaker361@gmail.com>
2025-06-23 15:46:12 -0400

    find oom?

---
+++ find_oom_jobs.py
@@ -0,0 +1,43 @@
+import os
+
+cwd=os.getcwd()
+
+
+cluster="amarel"
+if cwd.find("donengel")!=-1:
+    cluster="chip"
+
+slurm_folder={
+    "amarel":"slurm",
+    "chip":"slurm_chip"
+}[cluster]
+
+dataset={
+    "amarel":"pers_league_captioned_splash",
+    "chip":"pers_art_coco_captioned"
+}[cluster]
+
+target_job_list=[]
+
+for root, dirs, files in os.walk(os.path.join(slurm_folder,dataset)):
+    for name in files:
+        if name.endswith("out"):
+            with open(os.path.join(root,name),"r") as read_file:
+                for line in read_file:
+                    if line.startswith("OOM"):
+                        print(name)
+                        target_job_list.append(name)
+
+command_list=[]
+
+for root, dirs, files in os.walk("scripts"):
+    for name in files:
+        if name.endswith("sh"):
+            with open(os.path.join(root,name),"r") as read_file:
+                for line in read_file:
+                    for target_job in target_job_list:
+                        if line.find(target_job)!=-1:
+                            command_list.append(line)
+
+for command in command_list:
+    print(command)

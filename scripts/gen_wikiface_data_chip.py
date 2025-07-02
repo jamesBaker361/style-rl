17bc16a20764c60cb3eb48627c3ff0f4649a73be
James Baker <jlbaker361@gmail.com>
2025-07-02 17:56:35 -0400

    gen all

--- scripts/gen_wikiface_data_chip.py
+++ scripts/gen_wikiface_data_chip.py
@@ -1,7 +1,13 @@
 for limit in [1000,5000,-1]:
     for pipeline in ["stability","lcm","sana"]:
         for embedding in ["siglip2","ssl","clip","dino"]:
-            name=f"{embedding}-wikifaces{limit}-{pipeline}"
-            command=f"sbatch -J npz --err=slurm_chip/npz/{name}.err --out=slurm_chip/npz/{name}.out runpygpu_chip.sh make_wikiart_dataset.py  --embedding {embedding} "
-            command+=f"  --output_dataset jlbaker361/{name} --limit {limit} --rewrite --pipeline {pipeline} "
-            print(command)
+            for data in ['wikiart',"realkaggle"]:
+                name=f"{embedding}-{data}{limit}-{pipeline}"
+                command=f"sbatch -J npz --err=slurm_chip/npz/{name}.err --out=slurm_chip/npz/{name}.out runpygpu_chip.sh make_wikiart_dataset.py  --embedding {embedding} "
+                command+=f"  --output_dataset jlbaker361/{name} --limit {limit} --rewrite --pipeline {pipeline} "
+                dataset_choice={
+                    "wikiart":" asahi417/wikiart-face ",
+                    "realkaggle":"kaustubhdhote/human-faces-dataset"
+                }[data]
+                command+=f" --dataset {dataset_choice}"
+                print(command)

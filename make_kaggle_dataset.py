4b36776c9f9626db0761d96cd57ca8304b1c533b
James Baker <jlbaker361@gmail.com>
2025-07-02 17:05:38 -0400

    kaggle dataset?

--- make_kaggle_dataset.py
+++ make_kaggle_dataset.py
@@ -1,6 +1,14 @@
 import kagglehub
+import os
 
 # Download latest version
 path = kagglehub.dataset_download("kaustubhdhote/human-faces-dataset")
 
 print("Path to dataset files:", path)
+real_images="Real Images"
+
+real_dir=os.path.join(path,real_images)
+
+jpg_files = [f for f in os.listdir(real_dir) if f.lower().endswith('.jpg')]
+
+print("len",len(jpg_files))

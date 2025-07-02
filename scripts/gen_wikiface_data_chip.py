48e8c3b8fbf54daf5b7666a160dc5ff3a02a8534
James Baker <jlbaker361@gmail.com>
2025-07-02 17:39:43 -0400

    -1

--- scripts/gen_wikiface_data_chip.py
+++ scripts/gen_wikiface_data_chip.py
@@ -1,7 +1,7 @@
 for limit in [1000,5000,-1]:
     for pipeline in ["stability","lcm","sana"]:
         for embedding in ["siglip2","ssl","clip","dino"]:
-            name=f"{embedding}-wikifaces-{limit}-{pipeline}"
+            name=f"{embedding}-wikifaces{limit}-{pipeline}"
             command=f"sbatch -J npz --err=slurm_chip/npz/{name}.err --out=slurm_chip/npz/{name}.out runpygpu_chip.sh make_np_dataset.py  --embedding {embedding} "
             command+=f"  --output_dataset jlbaker361/{name} --limit {limit} --rewrite --pipeline {pipeline} "
             print(command)

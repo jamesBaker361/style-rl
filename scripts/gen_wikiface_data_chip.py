79cecd4938bc6abfeb1b89d569334ea6a7471e48
James Baker <jlbaker361@gmail.com>
2025-07-03 10:15:41 -0400

    dont rewrite

--- scripts/gen_wikiface_data_chip.py
+++ scripts/gen_wikiface_data_chip.py
@@ -4,7 +4,7 @@ for limit in [1000,5000,-1]:
             for data in ['wikiart',"realkaggle"]:
                 name=f"{embedding}-{data}{limit}-{pipeline}"
                 command=f"sbatch -J npz --err=slurm_chip/npz/{name}.err --out=slurm_chip/npz/{name}.out runpygpu_chip.sh make_wikiart_dataset.py  --embedding {embedding} "
-                command+=f"  --output_dataset jlbaker361/{name} --limit {limit} --rewrite --pipeline {pipeline} "
+                command+=f"  --output_dataset jlbaker361/{name} --limit {limit}  --pipeline {pipeline} "
                 dataset_choice={
                     "wikiart":" asahi417/wikiart-face ",
                     "realkaggle":"kaustubhdhote/human-faces-dataset"

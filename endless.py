e474a0022408266b19145b9a2dc0d1d291a48f1e
James Baker <jlbaker361@gmail.com>
2025-07-06 18:16:23 -0400

    only when no pending jobs

--- endless.py
+++ endless.py
@@ -6,22 +6,41 @@ import os
 
 gpu_details.print_details()
 
+def get_pending_job_ids(user):
+    result = subprocess.run(
+        ["squeue", "-u", user, "-t", "PENDING", "-h", "-o", "%A"],
+        capture_output=True,
+        text=True,
+        check=True
+    )
+    job_ids = result.stdout.strip().split("\n")
+    return [jid for jid in job_ids if jid]
+
 def run_job():
     print(f"[{datetime.now()}] Running job...")
 
-    # Step 1: Run find_oom_jobs.py and save output to oom.sh
-    with open("oom.sh", "w") as f:
-        subprocess.run(["python", "find_oom_jobs.py"], stdout=f, check=True)
-    
-    # Step 2: Run the generated oom.sh script
-    subprocess.run(["bash", "oom.sh"], check=True)
+    if os.getcwd().find("jlb638")!=-1:
+        pending=get_pending_job_ids("jlb638")
+    else:
+        pending=get_pending_job_ids("jbaker15")
+
+    print("penidnign jobs: ", len(pending))
+    if len(pending)==0:
+        # Step 1: Run find_oom_jobs.py and save output to oom.sh
+        with open("oom.sh", "w") as f:
+            subprocess.run(["python", "find_oom_jobs.py"], stdout=f, check=True)
+        
+        # Step 2: Run the generated oom.sh script
+        subprocess.run(["bash", "oom.sh"], check=True)
 
     print(f"[{datetime.now()}] Job completed.")
+
+
 
 
 
 count=0
 while True:
     run_job()
-    time.sleep(6 * 60 * 60)  # Wait for 6 hours
+    time.sleep(4 * 60 * 60)  # Wait for 6 hours
     count+=1

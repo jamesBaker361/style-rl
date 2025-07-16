a8131df5db6c44196af76f17318c3a6afea777f7
James Baker <jlbaker361@gmail.com>
2025-07-16 11:48:21 -0400

    priting

--- scale_prediction.py
+++ scale_prediction.py
@@ -347,6 +347,9 @@ def main(args):
 
     @torch.no_grad()
     def forward(unet,noise:torch.Tensor,embedding,encoder_hidden_states,scheduler,num_inference_steps:int)->torch.Tensor:
+        accelerator.print("logging image_batches",noise.size())
+        accelerator.print("logging embeddings",embedding.size())
+        accelerator.print("logging encoder",encoder_hidden_states.size())
         added_cond_kwargs={"image_embeds":embedding}
         scheduler.set_timesteps(num_inference_steps, device=device)
         timesteps = scheduler.timesteps

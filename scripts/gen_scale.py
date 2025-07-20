30383998770c0584cf86a86e74bf896da8fac483
James Baker <jlbaker361@gmail.com>
2025-07-20 15:50:29 -0400

    name

--- scripts/gen_scale.py
+++ scripts/gen_scale.py
@@ -6,5 +6,5 @@ for lr in [0.001,0.0001]:
                 command=f"sbatch -J scale --err=slurm/scale/{name}.err --out=slurm/scale/{name}.out runpygpu.sh  scale_prediction.py "
                 command+=f" --epochs 100  --validation_interval 5 --identity_adapter --train_split 0.95 --limit -1"
                 command+=f" --batch_size 2 --gradient_accumulation_steps 16 --project_name {data}-{n}-scale "
-                command+=f" --dataset jlbaker361/{embedding}-{data}-{n} --lr {lr} --embedding {embedding}  --use_lora --load "
+                command+=f" --dataset jlbaker361/{embedding}-{data}-{n} --lr {lr} --embedding {embedding}  --use_lora --load --name {name} "
                 print(command)

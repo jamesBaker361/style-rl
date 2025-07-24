30c221ca170d64a35f23c02926679caea4304efc
James Baker <jlbaker361@gmail.com>
2025-07-24 17:49:21 -0400

    np for lcm

--- scripts/gen_np.py
+++ scripts/gen_np.py
@@ -1,7 +1,7 @@
 for embedding in ["siglip2","ssl","clip","dino"]:
-    for data in ["league_captioned_splash","coco_captioned","art_coco_captioned"]:
-        for pipeline in ["sana"]:
-            for limit in [1]:
+    for data in ["league_captioned_splash"]:
+        for pipeline in ["lcm"]:
+            for limit in [1000,250]:
                 name=f"{embedding}-{data}-{limit}-{pipeline}"
                 command=f"sbatch -J npz --err=slurm/npz/{name}.err --out=slurm/npz/{name}.out runpygpu.sh make_np_dataset.py  --embedding {embedding} "
                 command+=f" --dataset jlbaker361/{data} --output_dataset jlbaker361/{name} --limit {limit}  --pipeline {pipeline} --rewrite --mixed_precision no "

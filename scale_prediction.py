953d927906a5733daaff9b81f907bba1d6ed9d1e
James Baker <jlbaker361@gmail.com>
2025-07-24 14:36:47 -0400

    printing

--- scale_prediction.py
+++ scale_prediction.py
@@ -424,6 +424,11 @@ def main(args):
                 lowres = F.interpolate(image_patches, scale_factor=down_scale_factor, mode='bilinear', align_corners=False)
                 upscaled = F.interpolate(lowres, scale_factor=up_scale_factor, mode='bilinear', align_corners=False)
 
+                if  b==0:
+                    print("size",encoder_hidden_states.size(),"embedding size",embedding_batch.size(),"img size",upscaled.size())
+                    print("device",encoder_hidden_states.device,"embedding device",embedding_batch.device,"img device",upscaled.device)
+                    print("text ",encoder_hidden_states.dtype,"embedding ",embedding_batch.dtype,"img ",upscaled.dtype,)
+
                 mini_batches=[
                     forward(unet,upscaled[i:i+args.batch_size],embeddings[i:i+args.batch_size],encoder_hidden_states[i:i+args.batch_size],scheduler,args.num_inference_steps )for i in range(0,((patch_size**2)//args.batch_size),args.batch_size) if upscaled[i:i+args.batch_size].size()[0]!=0
                 ]

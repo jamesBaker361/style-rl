1a6823bf534663772e341f3a7816021e7b9481d4
James Baker <jlbaker361@gmail.com>
2025-07-24 22:52:34 -0400

    split

--- scripts/gen_scale_mini.py
+++ scripts/gen_scale_mini.py
@@ -4,7 +4,7 @@ for lr in [0.001,0.0001]:
             for n in [1000]:
                 name=f"{embedding}_miniscale_{lr}_{data}_{n}"
                 command=f"sbatch -J scale --err=slurm/scale_mini/{name}.err --out=slurm/scale_mini/{name}.out runpygpu.sh  scale_prediction.py "
-                command+=f" --epochs 100  --validation_interval 2 --identity_adapter --train_split 0.95 --limit 10"
+                command+=f" --epochs 100  --validation_interval 2 --identity_adapter --train_split 0.5 --limit 10"
                 command+=f" --batch_size 2 --gradient_accumulation_steps 16 --project_name {data}-{n}-scale "
                 command+=f" --dataset jlbaker361/{embedding}-{data}-{n} --lr {lr} --embedding {embedding}  --use_lora --load --name {name} "
                 print(command)

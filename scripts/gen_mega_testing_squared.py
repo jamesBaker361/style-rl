5f1d2c4f8cbd2456dd42257ee41f6bd626dc3936
James Baker <jlbaker361@gmail.com>
2025-07-30 16:55:32 -0400

    0.1

--- scripts/gen_mega_testing_squared.py
+++ scripts/gen_mega_testing_squared.py
@@ -17,7 +17,7 @@ for training_type in  ["denoise"]: #,"reward","latents_reward"]:
                                         for flag in ["zeros","decreasing_scale"]:
                                             command=f"sbatch  -J perstest  --err=slurm/perstesting_{data}/{flag}_{name}.err --out=slurm/perstesting_{data}/{flag}_{name}.out --gres=gpu:1 "
                                             command+=f" runaccgpu.sh  --mixed_precision fp16 --num_processes 1 --main_process_port {port} main_logging.py --epochs 500 --limit 10 --batch_size 1 --project_name {data}-{n}-testing "
-                                            command+=f" --mixed_precision fp16 --prediction_type {prediction_type} --upload_interval 1 --uncaptioned_frac {frac} --train_split 0.5 --lr {lr} --load --generic_test_prompts "
+                                            command+=f" --mixed_precision fp16 --prediction_type {prediction_type} --upload_interval 1 --uncaptioned_frac {frac} --train_split 0.1 --lr {lr} --load --generic_test_prompts "
                                             command+=f" --embedding {embedding} --training_type {training_type} --dataset jlbaker361/{embedding}-{data}-{n} --vanilla --name jlbaker361/{name} --gradient_accumulation_steps 8  "
                                             command+=f" --pipeline {pipeline} --reward_switch_epoch {reward_switch_epoch}   --real_test_prompts --{flag} "
                                             if training_type=="reward":

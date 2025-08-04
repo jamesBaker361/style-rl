c1f43f010faf86e12cd229b49ce4d9bd5bd356ae
James Baker <jlbaker361@gmail.com>
2025-08-04 17:35:51 -0400

    increasing

--- main_logging.py
+++ main_logging.py
@@ -130,6 +130,7 @@ parser.add_argument("--sigma_data",type=float,default=-0.8)
 parser.add_argument("--real_test_prompts",action="store_true")
 parser.add_argument("--zeros",action="store_true")
 parser.add_argument("--decreasing_scale",action="store_true")
+parser.add_argument("--increasing_scale",action="store_true")
 parser.add_argument("--fid",action="store_true")
 
 import torch
@@ -535,7 +536,7 @@ def main(args):
             #test_prompt_list[k]=generic_str_list[k%len(generic_str_list)]
             test_text_list[k]=generic_tensor_list[k%len(generic_str_list)]
 
-    if args.zeros:
+    if args.zeros or args.increasing_scale:
         pipeline.set_ip_adapter_scale(0.0)
 
     for name, data_list in zip(["train","test","val"],[image_list,test_image_list,val_image_list]):
@@ -716,7 +717,7 @@ def main(args):
                                     num_inference_steps=args.num_inference_steps,
                                     #prompt_embeds=text_batch,
                                     ip_adapter_image_embeds=image_embeds,negative_prompt_embeds=batched_negative_prompt_embeds,
-                                    output_type="pt",height=args.image_size,width=args.image_size,decreasing_scale=args.decreasing_scale).images
+                                    output_type="pt",height=args.image_size,width=args.image_size,decreasing_scale=args.decreasing_scale,increasing_scale=args.increasing_scale).images
             
             #normal_image_set=pipeline(prompt_embeds=text_batch,output_type="pil").images
             image_batch=F_v2.resize(image_batch, (args.image_size,args.image_size))

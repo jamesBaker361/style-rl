5f62774f377a5bfaad867f8bc52448b540fae157
James Baker <jlbaker361@gmail.com>
2025-08-06 16:03:46 -0400

    more steps

--- mpgd.py
+++ mpgd.py
@@ -307,13 +307,15 @@ if __name__=="__main__":
     target_image=load_image("https://i.guim.co.uk/img/media/327aa3f0c3b8e40ab03b4ae80319064e401c6fbc/377_133_3542_2834/master/3542.jpg?width=1200&height=1200&quality=85&auto=format&fit=crop&s=34d32522f47e4a67286f9894fc81c863")
     target=pipeline.image_processor.preprocess(target_image,dim,dim).to("cuda")
 
+    steps=32
+
     print('target size',target.size())
     generator=torch.Generator(pipeline.unet.device)
     generator.manual_seed(123)
-    image=call_with_grad_and_guidance(pipeline,"cat",256,256,target=target,generator=generator).images[0]
+    image=call_with_grad_and_guidance(pipeline,"cat",256,256,target=target,generator=generator,num_inference_steps=steps).images[0]
     image.save("grad_cat.png")
 
     generator=torch.Generator(pipeline.unet.device)
     generator.manual_seed(123)
-    image=call_with_grad_and_guidance(pipeline,"cat",256,256,generator=generator).images[0]
+    image=call_with_grad_and_guidance(pipeline,"cat",256,256,generator=generator,num_inference_steps=steps).images[0]
     image.save("cat.png")

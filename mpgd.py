640a743788894eb20e95668bb9417523bca25a6d
James Baker <jlbaker361@gmail.com>
2025-08-06 17:09:43 -0400

    bigger

--- mpgd.py
+++ mpgd.py
@@ -309,7 +309,7 @@ def call_with_grad_and_guidance(
 
 if __name__=="__main__":
     pipeline=CompatibleLatentConsistencyModelPipeline.from_pretrained("SimianLuo/LCM_Dreamshaper_v7").to("cuda")
-    dim=128
+    dim=256
     target_image=load_image("https://media.vogue.fr/photos/5c8a55363d44a0083ccbef54/2:3/w_2560%2Cc_limit/GettyImages-625257378.jpg")
     target_tensor=pipeline.image_processor.preprocess(target_image,dim,dim).to("cuda")
 
@@ -331,6 +331,6 @@ if __name__=="__main__":
         
         concat_image=concat_images_horizontally([image,grad_image])
 
-        concat_image.save(f"concat.png")
+        concat_image.save(f"concat_{steps}.png")
 
         print(f"all done {steps} ")

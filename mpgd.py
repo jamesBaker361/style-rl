d1b57468e316012a8793c87da76c786945d77e3c
James Baker <jlbaker361@gmail.com>
2025-08-06 14:59:35 -0400

    tsting

--- mpgd.py
+++ mpgd.py
@@ -237,7 +237,7 @@ def call_with_grad_and_guidance(
 
             guidance_scale=10.0
             with torch.enable_grad():
-                decoded=self.vae.decode(denoised)
+                decoded=self.vae.decode(denoised).sample
                 diff=torch.nn.functional.mse_loss(decoded,target)
 
                 diff_gradient=torch.autograd.grad(outputs=diff,inputs=decoded)
@@ -305,6 +305,8 @@ if __name__=="__main__":
     dim=256
     target_image=load_image("https://i.guim.co.uk/img/media/327aa3f0c3b8e40ab03b4ae80319064e401c6fbc/377_133_3542_2834/master/3542.jpg?width=1200&height=1200&quality=85&auto=format&fit=crop&s=34d32522f47e4a67286f9894fc81c863")
     target=pipeline.image_processor.preprocess(target_image,dim,dim)
+
+    print('target size',target.size())
 
     image=call_with_grad_and_guidance(pipeline,"cat",256,256,target=target).images[0]
     image.save("generated_cat.png")

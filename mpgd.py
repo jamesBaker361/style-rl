2121944bd6479cbebe91ba2c9e81d4bc039dc8b7
James Baker <jlbaker361@gmail.com>
2025-08-06 20:57:55 -0400

    big

--- mpgd.py
+++ mpgd.py
@@ -650,7 +650,7 @@ if __name__=="__main__":
     pipeline=StableDiffusionPipeline.from_pretrained("stabilityai/stable-diffusion-2-1",dtype=torch.float16).to("cuda")
     #pipeline.scheduler=CompatibleDDIMScheduler.from_config(pipeline.scheduler.config)
     pipeline.vae.requires_grad_(False)
-    dim=128
+    dim=256
     target_image=load_image("https://media.vogue.fr/photos/5c8a55363d44a0083ccbef54/2:3/w_2560%2Cc_limit/GettyImages-625257378.jpg")
     target_tensor=pipeline.image_processor.preprocess(target_image,dim,dim).to("cuda",dtype=torch.float16,)
 

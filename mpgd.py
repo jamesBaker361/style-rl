f5103f6239fca562cf77b9f8d128a811e383d852
James Baker <jlbaker361@gmail.com>
2025-08-06 20:33:22 -0400

    grad stuff

--- mpgd.py
+++ mpgd.py
@@ -418,136 +418,136 @@ def ddim_call_with_guidance(
             second element is a list of `bool`s indicating whether the corresponding generated image contains
             "not-safe-for-work" (nsfw) content.
     """
+    with torch.no_grad():
+        callback = kwargs.pop("callback", None)
+        callback_steps = kwargs.pop("callback_steps", None)
 
-    callback = kwargs.pop("callback", None)
-    callback_steps = kwargs.pop("callback_steps", None)
-
-    if callback is not None:
-        deprecate(
-            "callback",
-            "1.0.0",
-            "Passing `callback` as an input argument to `__call__` is deprecated, consider using `callback_on_step_end`",
-        )
-    if callback_steps is not None:
-        deprecate(
-            "callback_steps",
-            "1.0.0",
-            "Passing `callback_steps` as an input argument to `__call__` is deprecated, consider using `callback_on_step_end`",
-        )
+        if callback is not None:
+            deprecate(
+                "callback",
+                "1.0.0",
+                "Passing `callback` as an input argument to `__call__` is deprecated, consider using `callback_on_step_end`",
+            )
+        if callback_steps is not None:
+            deprecate(
+                "callback_steps",
+                "1.0.0",
+                "Passing `callback_steps` as an input argument to `__call__` is deprecated, consider using `callback_on_step_end`",
+            )
 
 
-    # 0. Default height and width to unet
-    if not height or not width:
-        height = (
-            self.unet.config.sample_size
-            if self._is_unet_config_sample_size_int
-            else self.unet.config.sample_size[0]
-        )
-        width = (
-            self.unet.config.sample_size
-            if self._is_unet_config_sample_size_int
-            else self.unet.config.sample_size[1]
-        )
-        height, width = height * self.vae_scale_factor, width * self.vae_scale_factor
-    # to deal with lora scaling and other possible forward hooks
+        # 0. Default height and width to unet
+        if not height or not width:
+            height = (
+                self.unet.config.sample_size
+                if self._is_unet_config_sample_size_int
+                else self.unet.config.sample_size[0]
+            )
+            width = (
+                self.unet.config.sample_size
+                if self._is_unet_config_sample_size_int
+                else self.unet.config.sample_size[1]
+            )
+            height, width = height * self.vae_scale_factor, width * self.vae_scale_factor
+        # to deal with lora scaling and other possible forward hooks
 
-    # 1. Check inputs. Raise error if not correct
-    self.check_inputs(
-        prompt,
-        height,
-        width,
-        callback_steps,
-        negative_prompt,
-        prompt_embeds,
-        negative_prompt_embeds,
-        ip_adapter_image,
-        ip_adapter_image_embeds,
-        callback_on_step_end_tensor_inputs,
-    )
-
-    self._guidance_scale = guidance_scale
-    self._guidance_rescale = guidance_rescale
-    self._clip_skip = clip_skip
-    self._cross_attention_kwargs = cross_attention_kwargs
-    self._interrupt = False
-
-    # 2. Define call parameters
-    if prompt is not None and isinstance(prompt, str):
-        batch_size = 1
-    elif prompt is not None and isinstance(prompt, list):
-        batch_size = len(prompt)
-    else:
-        batch_size = prompt_embeds.shape[0]
-
-    device = self._execution_device
-
-    # 3. Encode input prompt
-    lora_scale = (
-        self.cross_attention_kwargs.get("scale", None) if self.cross_attention_kwargs is not None else None
-    )
-
-    prompt_embeds, negative_prompt_embeds = self.encode_prompt(
-        prompt,
-        device,
-        num_images_per_prompt,
-        self.do_classifier_free_guidance,
-        negative_prompt,
-        prompt_embeds=prompt_embeds,
-        negative_prompt_embeds=negative_prompt_embeds,
-        lora_scale=lora_scale,
-        clip_skip=self.clip_skip,
-    )
-
-    # For classifier free guidance, we need to do two forward passes.
-    # Here we concatenate the unconditional and text embeddings into a single batch
-    # to avoid doing two forward passes
-    if self.do_classifier_free_guidance:
-        prompt_embeds = torch.cat([negative_prompt_embeds, prompt_embeds])
-
-    if ip_adapter_image is not None or ip_adapter_image_embeds is not None:
-        image_embeds = self.prepare_ip_adapter_image_embeds(
+        # 1. Check inputs. Raise error if not correct
+        self.check_inputs(
+            prompt,
+            height,
+            width,
+            callback_steps,
+            negative_prompt,
+            prompt_embeds,
+            negative_prompt_embeds,
             ip_adapter_image,
             ip_adapter_image_embeds,
-            device,
-            batch_size * num_images_per_prompt,
-            self.do_classifier_free_guidance,
+            callback_on_step_end_tensor_inputs,
         )
 
-    # 4. Prepare timesteps
-    timesteps, num_inference_steps = retrieve_timesteps(
-        self.scheduler, num_inference_steps, device, timesteps, sigmas
-    )
+        self._guidance_scale = guidance_scale
+        self._guidance_rescale = guidance_rescale
+        self._clip_skip = clip_skip
+        self._cross_attention_kwargs = cross_attention_kwargs
+        self._interrupt = False
 
-    # 5. Prepare latent variables
-    num_channels_latents = self.unet.config.in_channels
-    latents = self.prepare_latents(
-        batch_size * num_images_per_prompt,
-        num_channels_latents,
-        height,
-        width,
-        prompt_embeds.dtype,
-        device,
-        generator,
-        latents,
-    )
+        # 2. Define call parameters
+        if prompt is not None and isinstance(prompt, str):
+            batch_size = 1
+        elif prompt is not None and isinstance(prompt, list):
+            batch_size = len(prompt)
+        else:
+            batch_size = prompt_embeds.shape[0]
 
-    # 6. Prepare extra step kwargs. TODO: Logic should ideally just be moved out of the pipeline
-    extra_step_kwargs = self.prepare_extra_step_kwargs(generator, eta)
+        device = self._execution_device
 
-    # 6.1 Add image embeds for IP-Adapter
-    added_cond_kwargs = (
-        {"image_embeds": image_embeds}
-        if (ip_adapter_image is not None or ip_adapter_image_embeds is not None)
-        else None
-    )
+        # 3. Encode input prompt
+        lora_scale = (
+            self.cross_attention_kwargs.get("scale", None) if self.cross_attention_kwargs is not None else None
+        )
 
-    # 6.2 Optionally get Guidance Scale Embedding
-    timestep_cond = None
-    if self.unet.config.time_cond_proj_dim is not None:
-        guidance_scale_tensor = torch.tensor(self.guidance_scale - 1).repeat(batch_size * num_images_per_prompt)
-        timestep_cond = self.get_guidance_scale_embedding(
-            guidance_scale_tensor, embedding_dim=self.unet.config.time_cond_proj_dim
-        ).to(device=device, dtype=latents.dtype)
+        prompt_embeds, negative_prompt_embeds = self.encode_prompt(
+            prompt,
+            device,
+            num_images_per_prompt,
+            self.do_classifier_free_guidance,
+            negative_prompt,
+            prompt_embeds=prompt_embeds,
+            negative_prompt_embeds=negative_prompt_embeds,
+            lora_scale=lora_scale,
+            clip_skip=self.clip_skip,
+        )
+
+        # For classifier free guidance, we need to do two forward passes.
+        # Here we concatenate the unconditional and text embeddings into a single batch
+        # to avoid doing two forward passes
+        if self.do_classifier_free_guidance:
+            prompt_embeds = torch.cat([negative_prompt_embeds, prompt_embeds])
+
+        if ip_adapter_image is not None or ip_adapter_image_embeds is not None:
+            image_embeds = self.prepare_ip_adapter_image_embeds(
+                ip_adapter_image,
+                ip_adapter_image_embeds,
+                device,
+                batch_size * num_images_per_prompt,
+                self.do_classifier_free_guidance,
+            )
+
+        # 4. Prepare timesteps
+        timesteps, num_inference_steps = retrieve_timesteps(
+            self.scheduler, num_inference_steps, device, timesteps, sigmas
+        )
+
+        # 5. Prepare latent variables
+        num_channels_latents = self.unet.config.in_channels
+        latents = self.prepare_latents(
+            batch_size * num_images_per_prompt,
+            num_channels_latents,
+            height,
+            width,
+            prompt_embeds.dtype,
+            device,
+            generator,
+            latents,
+        )
+
+        # 6. Prepare extra step kwargs. TODO: Logic should ideally just be moved out of the pipeline
+        extra_step_kwargs = self.prepare_extra_step_kwargs(generator, eta)
+
+        # 6.1 Add image embeds for IP-Adapter
+        added_cond_kwargs = (
+            {"image_embeds": image_embeds}
+            if (ip_adapter_image is not None or ip_adapter_image_embeds is not None)
+            else None
+        )
+
+        # 6.2 Optionally get Guidance Scale Embedding
+        timestep_cond = None
+        if self.unet.config.time_cond_proj_dim is not None:
+            guidance_scale_tensor = torch.tensor(self.guidance_scale - 1).repeat(batch_size * num_images_per_prompt)
+            timestep_cond = self.get_guidance_scale_embedding(
+                guidance_scale_tensor, embedding_dim=self.unet.config.time_cond_proj_dim
+            ).to(device=device, dtype=latents.dtype)
 
     # 7. Denoising loop
     num_warmup_steps = len(timesteps) - num_inference_steps * self.scheduler.order

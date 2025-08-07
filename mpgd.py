ed0ac7b9aad69ef6c968e9157a35f515af2847b1
James Baker <jlbaker361@gmail.com>
2025-08-06 20:22:08 -0400

    ddim pipe

--- mpgd.py
+++ mpgd.py
@@ -646,8 +646,8 @@ def ddim_call_with_guidance(
 
 
 if __name__=="__main__":
-    pipeline=CompatibleLatentConsistencyModelPipeline.from_pretrained("stabilityai/stable-diffusion-2-1").to("cuda")
-    pipeline.scheduler=CompatibleDDIMScheduler.from_config(pipeline.scheduler.config)
+    pipeline=StableDiffusionPipeline.from_pretrained("stabilityai/stable-diffusion-2-1").to("cuda")
+    #pipeline.scheduler=CompatibleDDIMScheduler.from_config(pipeline.scheduler.config)
     pipeline.vae.requires_grad_(False)
     dim=256
     target_image=load_image("https://media.vogue.fr/photos/5c8a55363d44a0083ccbef54/2:3/w_2560%2Cc_limit/GettyImages-625257378.jpg")

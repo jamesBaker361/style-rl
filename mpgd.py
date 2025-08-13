1d2577818919fcf24992baea5b5e997f70bb6751
James Baker <jlbaker361@gmail.com>
2025-08-13 15:28:51 -0400

    bigger scale

--- mpgd.py
+++ mpgd.py
@@ -968,7 +968,7 @@ if __name__=="__main__":
             base_image.save(f"images/base_{steps}.png")
             base_denoised_list=concat_images_horizontally(denoised_list)
             base_denoised_list.save(f"images/base_concat_{steps}.png")
-            for guidance_strength in [-0.1,0.1]:
+            for guidance_strength in [-1,1,-10,10]:
                 for guidance_steps in [1,5,10]:
                 
                     for k,v in prompt_dict.items():

cf7ed266774e05390d196a31ca300519c9c5aed6
James Baker <jlbaker361@gmail.com>
2025-08-21 21:34:29 -0400

    train

--- make_pca_npz.py
+++ make_pca_npz.py
@@ -5,6 +5,9 @@ from datasets import load_dataset
 for embedding in ["clip","dino","ssl","siglip2"]:
     data=load_dataset(f"jlbaker361/{embedding}-league_captioned_splash-1000",split="train")
     X_data=[np.array(row["embedding"])[0][0] for row in data]
+    frac=0.95
+    cutoff=int(frac*len(X_data))
+    X_data=X_data[:cutoff]
     pca_object=PCA(n_components=100)
     pca_object.fit(X_data)
-    np.savez(f"{embedding}_pca.npz",components_=pca_object.components_, explained_variance_=pca_object.explained_variance_)
+    np.savez(f"{embedding}_pca_{frac}.npz",components_=pca_object.components_, explained_variance_=pca_object.explained_variance_)

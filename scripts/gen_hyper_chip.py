3442c9da4c78b2225cac6ac6f7bb2681ad333ff7
James Baker <jlbaker361@gmail.com>
2025-08-25 18:49:02 -0400

    hyper plane

--- scripts/gen_hyper_chip.py
+++ scripts/gen_hyper_chip.py
@@ -1,3 +1,4 @@
 for embedding in ["clip","ssl","siglip2","dino"]:
     command=f"sbatch -J hyper --err=slurm_chip/{embedding}.err --out=slurm_chip/{embedding}.out runpygpu_chip.sh hyper_plane.py "
     command+=f" --src_dataset jlbaker361/league-tagged-{embedding} --dest_dataset jlbaker361/league-{embedding}-classification"
+    print(command)

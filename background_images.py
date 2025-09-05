1b43c9c7302f05364757c176619aba3dc0d5b11d
James Baker <jlbaker361@gmail.com>
2025-09-05 19:10:50 -0400

    background

---
+++ background_images.py
@@ -0,0 +1,71 @@
+
+real_test_prompt_list=[
+           ' in the jungle',
+            ' in the snow',
+            ' on the beach',
+            ' on a cobblestone street',
+            ' on top of pink fabric',
+            ' on top of a wooden floor',
+            ' with a city in the background',
+            ' with a mountain in the background',
+            ' with a blue house in the background',
+            ' on top of a purple rug in a forest',
+            ' with a wheat field in the background',
+            ' with a tree and autumn leaves in the background',
+            ' with the Eiffel Tower in the background',
+            ' floating on top of water',
+            ' floating in an ocean of milk',
+            ' on top of green grass with sunflowers around it',
+            ' on top of a mirror',
+            ' on top of the sidewalk in a crowded street',
+            ' on top of a dirt road',
+            ' on top of a white rug',
+        ]
+
+import os
+import argparse
+from experiment_helpers.gpu_details import print_details
+from accelerate import Accelerator
+import time
+import torch
+from diffusers import StableDiffusionPipeline
+from datasets import load_dataset,Dataset
+
+parser=argparse.ArgumentParser()
+
+parser.add_argument("--mixed_precision",type=str,default="no")
+
+
+
+def main(args):
+    accelerator=Accelerator()
+    steps=32
+    size=512
+    data_dict={
+        "image":[],
+        "prompt":[]
+    }
+    pipe = StableDiffusionPipeline.from_pretrained(
+            "SimianLuo/LCM_Dreamshaper_v7",
+            torch_dtype=torch.float16,
+        ).to(accelerator.device)
+    for prompt in real_test_prompt_list:
+        gen=torch.Generator()
+        gen.manual_seed(10101)
+        image=pipe(prompt, size,size,steps,generator=gen).images[0]
+        data_dict['image'].append(image)
+        data_dict['prompt'].append(prompt)
+
+    Dataset.from_dict(data_dict).push_to_hub("jlbaker361/real_test_prompt_list")
+
+if __name__=='__main__':
+    print_details()
+    start=time.time()
+    args=parser.parse_args()
+    print(args)
+    main(args)
+    end=time.time()
+    seconds=end-start
+    hours=seconds/(60*60)
+    print(f"successful generating:) time elapsed: {seconds} seconds = {hours} hours")
+    print("all done!")

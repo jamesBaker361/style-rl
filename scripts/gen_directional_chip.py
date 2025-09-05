fb0b86e587dd39731063c1018a86469ccd3b51f0
James Baker <jlbaker361@gmail.com>
2025-09-05 15:35:14 -0400

    directional

---
+++ scripts/gen_directional_chip.py
@@ -0,0 +1,4 @@
+for seed_offset in [x *10 for x in range(10)]:
+    command=f"sbatch -J dir --err=slurm_chip/direction/{seed_offset}.err --out=slurm_chip/direction/{seed_offset}.out "
+    command+=f" runpygpu_chip.sh directional_data.py --seed_offset {seed_offset} --dest_dataset jlbaker361/directional-{seed_offset} "
+    print(command)

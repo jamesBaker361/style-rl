9723c0309c013cfc3171400dcc892a03a9068b8a
James Baker <jlbaker361@gmail.com>
2025-09-05 19:25:03 -0400

    mtg

--- scripts/gen_overlap.py
+++ scripts/gen_overlap.py
@@ -15,7 +15,7 @@ for overlap_frac in [0.5,0.75,0.25]:
                             final_adapter_steps_list=step_dict[step_set]
                         else:
                             final_adapter_steps_list=" 0 1 2 3 4 5 6 7 "
-                        command=f"sbatch --exclude=gpu[005,006,008,010,011,013,014,018],cuda[001-008],pascal[006-010],gpuk[001-012] --err=slurm/overlap/_{name}.err --out=slurm/overlap/_{name}.out runpygpu.sh main_seg.py --segmentation_attention_method overlap --initial_mask_step_list 1 2 --final_mask_steps_list {final_mask_steps_list} --final_adapter_steps_list {final_adapter_steps_list} --limit 32 --overlap_frac {overlap_frac} "
+                        command=f"sbatch -J seg --exclude=gpu[005,006,008,010,011,013,014,018],cuda[001-008],pascal[006-010],gpuk[001-012] --err=slurm/overlap/_{name}.err --out=slurm/overlap/_{name}.out runpygpu.sh main_seg.py --segmentation_attention_method overlap --initial_mask_step_list 1 2 --final_mask_steps_list {final_mask_steps_list} --final_adapter_steps_list {final_adapter_steps_list} --limit 32 --overlap_frac {overlap_frac} "
                         command+=f" --kv_type {kv_type} "
                         if kv_type=="str":
                             command+=f" --token 0 "

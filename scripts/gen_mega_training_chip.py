239d009bd9e92d7893b3097c047287699c54c3e1
James Baker <jlbaker361@gmail.com>
2025-09-18 20:23:46 -0400

    args

--- scripts/gen_mega_training_chip.py
+++ scripts/gen_mega_training_chip.py
@@ -15,7 +15,7 @@ for training_type in  ["denoise"]: #,"reward","latents_reward"]:
                                         name=f"{training_type}_{prediction_type}_{embedding}_{frac}_{lr}_{n}{suffix}_{pipeline}_{reward_switch_epoch}"
                                         port+=1
                                         command=f"sbatch  -J pers  --err=slurm_chip/pers2_{data}/{name}.err --out=slurm_chip/pers2_{data}/{name}.out --gres=gpu:1 "
-                                        command+=f" runpygpu_chip.sh  --mixed_precision fp16 --num_processes 1 --main_process_port {port} main_pers.py --epochs 1000 --limit -1 --batch_size 2 --project_name {data}-{n}-beta "
+                                        command+=f" runpygpu_chip.sh   main_pers.py --epochs 1000 --limit -1 --batch_size 2 --project_name {data}-{n}-beta "
                                         command+=f" --mixed_precision fp16 --prediction_type {prediction_type} --upload_interval 1 --uncaptioned_frac {frac} --train_split 0.95 --lr {lr} --load --generic_test_prompts "
                                         command+=f" --embedding {embedding} --training_type {training_type} --dataset jlbaker361/{embedding}-{data}-{n} --vanilla --name jlbaker361/{name} --gradient_accumulation_steps 8  "
                                         command+=f" --pipeline {pipeline} --reward_switch_epoch {reward_switch_epoch} --load_hf "

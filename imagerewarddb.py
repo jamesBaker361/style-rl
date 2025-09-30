88f3a6236856f981652c6fd6b59c4ce980f7d6e4
James <jlbaker361@gmail.com>
2025-09-29 22:03:23 -0400

    cleanup

--- imagerewarddb.py
+++
@@ -1,7 +0,0 @@
-from datasets import load_dataset
-
-# Load the 1K-scale dataset
-dataset = load_dataset("THUDM/ImageRewardDB", "1k",trust_remote_code=True)
-
-for batch in dataset:
-    print(batch)

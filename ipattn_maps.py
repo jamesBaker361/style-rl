955907dd1f5289e351ece15790f76e06ee2a21eb
James Baker <jlbaker361@gmail.com>
2025-10-02 20:57:24 -0400

    i forget

--- ipattn_maps.py
+++ ipattn_maps.py
@@ -296,8 +296,7 @@ if __name__ =="__main__":
 
             segmented=sam(gen_image,dim,dim)
 
-            left=concat_images_vertically([add_caption_below_image_obj(ip_adapter_image, "source image",font_size=60,font_path="aileron_regular.otf"),
-                                           add_caption_below_image_obj(gen_image,"generated image",font_size=40,font_path="aileron_regular.otf"),
+            left=concat_images_vertically([ip_adapter_image, gen_image
                                            #segmented
                                            ])
 

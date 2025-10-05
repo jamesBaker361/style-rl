af52d15020f3bcf7e9a04fe318aa34e1595f1686
James Baker <jlbaker361@gmail.com>
2025-10-05 18:39:40 -0400

    test

--- scripts/gen_mega_training_test.py
+++ scripts/gen_mega_training_test.py
@@ -14,7 +14,7 @@ for training_type in  ["denoise"]: #,"reward","latents_reward"]:
                         
                                         name=f"{training_type}_{prediction_type}_{embedding}_{frac}_{lr}_{n}{suffix}_{pipeline}_{reward_switch_epoch}"
                                         port+=1
-                                        command=f"sbatch  -J pers  --err=slurm/pers2_{data}/{name}.err --out=slurm/pers2_{data}/{name}.out --gres=gpu:1 "
+                                        command=f"sbatch  -J pers  --err=slurm/test_{data}/{name}.err --out=slurm/test_{data}/{name}.out --gres=gpu:1 "
                                         command+=f" runpygpu.sh   main_pers_test.py --epochs 1000 --limit -1 --batch_size 2 --project_name {data}-{n}-beta "
                                         command+=f" --mixed_precision fp16 --prediction_type {prediction_type} --upload_interval 1 --uncaptioned_frac {frac} --train_split 0.95 --lr {lr} --generic_test_prompts "
                                         command+=f" --embedding {embedding} --training_type {training_type} --dataset jlbaker361/{embedding}-{data}-{n} --vanilla --name jlbaker361/{name} --gradient_accumulation_steps 8  "
